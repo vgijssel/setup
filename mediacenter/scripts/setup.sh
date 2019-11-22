556b35cbcb6a9c27f3ec2ed3482d58d8f9f04cfd
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2019-11-22 09:14:43 +0100

    Replace setup script with Ansible playbook

--- mediacenter/scripts/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -eux
-
-# Add vagrant user to sudoers for password-less sudo
-# https://www.vagrantup.com/docs/boxes/base.html#password-less-sudo
-echo "vagrant        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
-
-# Remove requiretty from /etc/sudoers if it exists
-sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
