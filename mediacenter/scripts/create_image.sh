61fe00217cbc8b810cc5ad52ca5abb9030d468ac
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2019-11-26 22:02:30 +0100

    Create the baremetal image

--- mediacenter/scripts/create_image.sh
+++ mediacenter/scripts/create_image.sh
@@ -10,7 +10,8 @@ export DIB_DEV_USER_SHELL=/bin/bash
 
 # simple-init is for networking
 # devuser is to be able to login into the machine for developing
-disk-image-create -o /images/image -t qcow2 --image-cache /cache debian vm simple-init
+disk-image-create -o /images/image -t qcow2 --image-cache /cache debian baremetal dhcp-all-interfaces devuser
 
 # Convert .qcow2 to .vmdk to be able to load the disk in VMware
 qemu-img convert -f qcow2 -O vmdk /images/image.qcow2 /images/image.vmdk
+
