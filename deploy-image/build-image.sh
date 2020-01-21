83eeea33f000797b725c91f01fd40f2a03393c4a
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-01-21 23:51:28 +0100

    Created script and job for deploy-image

---
+++ deploy-image/build-image.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -Eeoux pipefail
+
+export DIB_RELEASE=buster
+export DIB_APT_MINIMAL_CREATE_INTERFACES=0
+export DEPLOY_IMAGE_DIR=$(readlink -f $(dirname "$0"))
+IMAGE_SHA_TAG=$(git rev-parse HEAD)
+
+docker run \
+       --rm \
+       -it \
+       --privileged \
+       -v "${DEPLOY_IMAGE_DIR}/images:/app/images" \
+       -v "${DEPLOY_IMAGE_DIR}/elements:/app/elements" \
+       --env DIB_RELEASE \
+       --env DIB_APT_MINIMAL_CREATE_INTERFACES \
+       "${IMAGE_BUILDER_IMAGE}:${IMAGE_SHA_TAG}" \
+       ramdisk-image-create -x -o images/deploy-image ramdisk debian hwdiscovery
