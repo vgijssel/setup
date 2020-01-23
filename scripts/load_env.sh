6011a7a0a15a7e6e5f8d036beaf596e8cdaf5931
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-01-23 21:47:27 +0100

    Use diffing in vars instead of setting everything

--- scripts/load_env.sh
+++ scripts/load_env.sh
@@ -2,27 +2,26 @@
 
 set -Eeou pipefail
 
+AFTER_VARS=
+BEFORE_VARS=
+BEFORE_VARS=$(compgen -v | tr '\n' ' ')
+
 source .envrc
 
-# get a list of all the environment variable names
-VARS=$(compgen -v)
+AFTER_VARS=$(compgen -v | tr '\n' ' ')
+
+# diffing before and after, so we only keep the vars defined in .envrc
+VARS=$(echo $BEFORE_VARS[@] $AFTER_VARS[@] | tr ' ' '\n' | sort | uniq -u)
+
+# adding PATH to the array to force an update
+VARS="$VARS PATH"
 
 # from: https://help.github.com/en/actions/automating-your-workflow-with-github-actions/development-tools-for-github-actions#set-an-environment-variable-set-env
 # setting environment variables in github actions
 
-# skip the following variables
-SKIP_VARS=(SHELLOPTS PPID PIPESTATUS OPTIND OPTERR IFS HISTCMD BASH_ARGC BASH_ARGV BASH_COMMAND BASH_LINENO BASH_SOURCE BASH_SUBSHELL)
-
 for var in $VARS; do
-  # if var is inside SKIP_VARS array
-  if [[ " ${SKIP_VARS[@]} " =~ " ${var} " ]]; then
-    # Skip rest of this particular loop iteration.
-    echo "SKIPPING: ${var}"
-    continue
-  fi
-
-    # from: https://stackoverflow.com/a/30205555
-    # if the variable $var does not exist
+  # from: https://stackoverflow.com/a/30205555
+  # if the variable $var does not exist
   if [ -z ${!var+x} ]; then
       # set an empty environment variable
       echo "::set-env name=$var::"
