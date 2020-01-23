dbb1c473df28e8991e2a87084df286bcbde7e5f4
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-01-23 14:37:15 +0100

    Created qemu_run.sh script for running qemu vms

--- scripts/qemu_run.sh
+++ scripts/qemu_run.sh
@@ -3,22 +3,61 @@
 set -Eeoux pipefail
 
 IMAGE_NAME="$1"
+IMAGE_FILE="$2"
+IMAGE_FILE_COPY_DIR="$SETUP_TMP_DIR/$IMAGE_NAME"
+IMAGE_FILE_COPY_FILE="$IMAGE_FILE_COPY_DIR/$IMAGE_NAME.qcow2"
+CLOUD_INIT_FILE="$IMAGE_FILE_COPY_DIR/cloud-init.iso"
+QEMU_IFUP="$SETUP_SCRIPT_DIR/qemu-ifup.sh"
+QEMU_IFDOWN="$SETUP_SCRIPT_DIR/qemu-ifdown.sh"
 
-echo "Booting image: '$@'"
+echo "Booting '$IMAGE_NAME' with disk '$IMAGE_FILE'"
 
 # Make sure we copy the harddisk, to not change the existing one
-cp -v "$@" images/testing.qcow2
+mkdir -p $IMAGE_FILE_COPY_DIR
+cp -v $IMAGE_FILE $IMAGE_FILE_COPY_FILE
 
 # Resize the image
-qemu-img resize images/testing.qcow2 32G
+qemu-img resize $IMAGE_FILE_COPY_FILE 32G
+
+# Create cloud-init user-data
+cat <<EOF | tee $IMAGE_FILE_COPY_DIR/user-data
+#cloud-config
+# openssl passwd -1 -salt SaltSalt debian
+# mkpasswd --method=sha-512 --rounds=4096 debian
+
+manage_etc_hosts: true
+users:
+  - name: debian
+    plain_text_passwd: debian
+    # So we can login at the console
+    lock_passwd: false
+    shell: /bin/bash
+    # Unrestricted sudo access
+    sudo: ALL=(ALL) NOPASSWD:ALL
+EOF
+
+# Create cloud-init meta-data
+cat <<EOF | tee $IMAGE_FILE_COPY_DIR/meta-data
+instance-id: iid-abcdefg
+local-hostname: $IMAGE_NAME
+EOF
+
+# Create cloud-init network-config
+cat <<EOF | tee $IMAGE_FILE_COPY_DIR/network-config
+version: 1
+config:
+- type: physical
+  name: ens3
+  subnets:
+    - type: dhcp
+EOF
 
 # Generate cloud-init image
 mkisofs \
-  -output cloud-init.iso \
+  -output $CLOUD_INIT_FILE \
   -volid cidata \
   -joliet \
-  -rock {cloud-init/user-data,cloud-init/meta-data,cloud-init/network-config}
-
+  -rock {$IMAGE_FILE_COPY_DIR/user-data,$IMAGE_FILE_COPY_DIR/meta-data,$IMAGE_FILE_COPY_DIR/network-config}
 
 # -serial stdio \
 # -nographic \
@@ -32,8 +71,8 @@ qemu-system-x86_64 \
   -smp 2 \
   -nographic \
   -accel hax \
-  -netdev tap,id=mynet0,script=qemu-ifup.sh,downscript=qemu-ifdown.sh \
+  -netdev tap,id=mynet0,script="$QEMU_IFUP",downscript="$QEMU_IFDOWN" \
   -device virtio-net,mac=52:54:00:0e:e0:65,netdev=mynet0 \
   -device virtio-rng-pci \
-  -drive file=images/testing.qcow2,if=virtio \
-  -drive file=cloud-init.iso,if=virtio
+  -drive file="$IMAGE_FILE_COPY_FILE",if=virtio \
+  -drive file="$CLOUD_INIT_FILE",if=virtio
