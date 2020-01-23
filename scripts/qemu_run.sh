d64b11eb480336e84d5976c9a3b9ad1556ff1cfa
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-01-23 17:24:54 +0100

    Created working test for kubernetes image

--- scripts/qemu_run.sh
+++ scripts/qemu_run.sh
@@ -9,6 +9,7 @@ IMAGE_FILE_COPY_FILE="$IMAGE_FILE_COPY_DIR/$IMAGE_NAME.qcow2"
 CLOUD_INIT_FILE="$IMAGE_FILE_COPY_DIR/cloud-init.iso"
 QEMU_IFUP="$SETUP_SCRIPT_DIR/qemu-ifup.sh"
 QEMU_IFDOWN="$SETUP_SCRIPT_DIR/qemu-ifdown.sh"
+QEMU_MAC=52:54:00:0e:e0:65
 
 echo "Booting '$IMAGE_NAME' with disk '$IMAGE_FILE'"
 
@@ -59,20 +60,21 @@ mkisofs \
   -joliet \
   -rock {$IMAGE_FILE_COPY_DIR/user-data,$IMAGE_FILE_COPY_DIR/meta-data,$IMAGE_FILE_COPY_DIR/network-config}
 
-# -serial stdio \
-# -nographic \
-# -nic user \
-# -accel hax \
-
-# Added -device virtio-rng-pci to prevent "random: crng init done" from taking long
+# -device virtio-rng-pci
+#         Prevent "random: crng init done" from taking a long time
+#
+# -serial mon:stdio
+#         In this mode, the virtual serial port and QEMU monitor are multiplexed onto stdio.
+#         And Ctrl+c is handled, i.e. QEMU won’t be terminated, and the signal will be passed to the guest.
 
 qemu-system-x86_64 \
   -m 2048 \
   -smp 2 \
-  -nographic \
+  -serial mon:stdio \
+  -display none \
   -accel hax \
   -netdev tap,id=mynet0,script="$QEMU_IFUP",downscript="$QEMU_IFDOWN" \
-  -device virtio-net,mac=52:54:00:0e:e0:65,netdev=mynet0 \
+  -device virtio-net,mac="$QEMU_MAC",netdev=mynet0 \
   -device virtio-rng-pci \
   -drive file="$IMAGE_FILE_COPY_FILE",if=virtio \
   -drive file="$CLOUD_INIT_FILE",if=virtio
