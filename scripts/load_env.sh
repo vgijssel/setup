93e22570fe427b79ea124127d03a969ae822f304
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-01-23 13:43:06 +0100

    Set env vars using echo statements

--- scripts/load_env.sh
+++ scripts/load_env.sh
@@ -12,11 +12,25 @@ source .envrc
 # compgen -v
 # echo "---------- AFTER ---------"
 
-echo "---------- AFTER ---------"
-echo $BASE_IMAGE
-echo $IMAGE_BUILDER_IMAGE
-echo $RAZOR_SERVER_IMAGE
-echo "---------- AFTER ---------"
+# echo "---------- AFTER ---------"
+# echo $BASE_IMAGE
+# echo $IMAGE_BUILDER_IMAGE
+# echo $RAZOR_SERVER_IMAGE
+# echo "---------- AFTER ---------"
 
+VARS=$(compgen -v)
 
-echo "::set-env name=BASE_IMAGE::yellow"
+# from: https://help.github.com/en/actions/automating-your-workflow-with-github-actions/development-tools-for-github-actions#set-an-environment-variable-set-env
+# setting environment variables in github actions
+
+for var in $VARS; do
+    # from: https://stackoverflow.com/a/30205555
+    # if the variable $var does not exist
+  if [ -z ${!var+x} ]; then
+      # set an empty environment variable
+      echo "::set-env name=$var::"
+  else
+      # set the value of $var in the env
+      echo "::set-env name=$var::${!var}"
+  fi
+done
