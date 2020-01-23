734d2dd704ca6e40bd5eac10879bffb74ba87cde
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-01-23 21:07:33 +0100

    Update setting env vars

--- scripts/load_env.sh
+++ scripts/load_env.sh
@@ -10,7 +10,17 @@ VARS=$(compgen -v)
 # from: https://help.github.com/en/actions/automating-your-workflow-with-github-actions/development-tools-for-github-actions#set-an-environment-variable-set-env
 # setting environment variables in github actions
 
+# skip the following variables
+SKIP_VARS=(SHELLOPTS PPID PIPESTATUS OPTIND OPTERR IFS HISTCMD BASH_ARGC BASH_ARGV BASH_COMMAND BASH_LINENO BASH_SOURCE BASH_SUBSHELL)
+
 for var in $VARS; do
+  # if var is inside SKIP_VARS array
+  if [[ " ${SKIP_VARS[@]} " =~ " ${var} " ]]; then
+    # Skip rest of this particular loop iteration.
+    echo "SKIPPING: ${var}"
+    continue
+  fi
+
     # from: https://stackoverflow.com/a/30205555
     # if the variable $var does not exist
   if [ -z ${!var+x} ]; then
