6e2f385675665720b0c667a189eddd000c18a508
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-02-05 15:00:50 +0100

    Force extlinux intead of grub2 when building kubernetes image

--- kubernetes/build-image.sh
+++ kubernetes/build-image.sh
@@ -4,6 +4,8 @@ set -Eeoux pipefail
 
 export DIB_RELEASE=buster
 export DIB_APT_MINIMAL_CREATE_INTERFACES=0
+# Force extlinux instead of grub2
+export DIB_EXTLINUX=1
 export KUBERNETES_DIR=$(readlink -f $(dirname "$0"))
 IMAGE_SHA_TAG=$(git rev-parse HEAD)
 
@@ -16,5 +18,6 @@ docker run \
        -v "${KUBERNETES_DIR}/elements:/app/elements" \
        --env DIB_RELEASE \
        --env DIB_APT_MINIMAL_CREATE_INTERFACES \
+       --env DIB_EXTLINUX \
        "${IMAGE_BUILDER_IMAGE}:${IMAGE_SHA_TAG}" \
        disk-image-create -x -o images/kubernetes_buster debian vm debian-networking-fix cloud-init-fix kubernetes growroot qemu-guest nfs resolvconf goss
