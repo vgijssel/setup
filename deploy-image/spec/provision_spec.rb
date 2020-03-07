d52062961c436549978eed98d8424744e4706013
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-03-07 15:08:18 +0100

    Created first spec for deploy-image

--- deploy-image/spec/provision_spec.rb
+++ deploy-image/spec/provision_spec.rb
@@ -1,5 +1,21 @@
+# frozen_string_literal: true
+
 describe 'Provision' do
-  it 'kerk' do
+  it 'writes the given disk_url to disk' do
+    disk_url = "http://#{ENV.fetch('IP_ADDRESS')}:8000/images/kubernetes/kubernetes_buster.qcow2"
+    target_disk = '/dev/vda'
+
+    task = run 'hivemind Procfile.tests',
+               timeout: 60,
+               extra_env: { DISK_URL: disk_url, TARGET_DISK: target_disk }
+
+    result = task.wait
+
+    expect(result.success?).to eq true
+    expect(result.stdout).to include("Start remote disk deploy")
+    expect(result.stdout).to include("Starting to download disk from '#{disk_url}'")
+    expect(result.stdout).to include("Writing 'disk.raw' to disk '#{target_disk}'")
+    expect(result.stdout).to include("Done writing disk!")
   end
 
   # it 'passes all tests on the master before creating a cluster' do
