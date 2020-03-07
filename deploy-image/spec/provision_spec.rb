ad8fe6931c10ec63a690c985f9bc82a7dc761804
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-03-07 13:54:31 +0100

    Basic rspec setup for deploy-image

---
+++ deploy-image/spec/provision_spec.rb
@@ -0,0 +1,36 @@
+describe 'Provision' do
+  # it 'passes all tests on the master before creating a cluster' do
+  #   result = ssh_master('validate_preflight', timeout: 60).wait
+
+  #   expect(result.exitstatus).to eq 0
+  #   expect(result.error).to be_nil
+  # end
+
+  # it 'passes all tests on the worker before creating a cluster' do
+  #   result = ssh_worker('validate_preflight', timeout: 60).wait
+
+  #   expect(result.exitstatus).to eq 0
+  #   expect(result.error).to be_nil
+  # end
+
+  # it 'successfully starts a kubernetes cluster with the master and worker' do
+  #   result = run("ansible-playbook -i hosts_test.ini setup_cluster.yml", timeout: 300).wait
+
+  #   expect(result.exitstatus).to eq 0
+  #   expect(result.error).to be_nil
+  # end
+
+  # it 'passes all tests on the master after creating a cluster' do
+  #   result = ssh_master('validate_master', timeout: 60).wait
+
+  #   expect(result.exitstatus).to eq 0
+  #   expect(result.error).to be_nil
+  # end
+
+  # it 'passes all tests on the worker after creating a cluster' do
+  #   result = ssh_worker('validate_worker', timeout: 60).wait
+
+  #   expect(result.exitstatus).to eq 0
+  #   expect(result.error).to be_nil
+  # end
+end
