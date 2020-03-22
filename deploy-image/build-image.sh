e8fb2c1d33341198dd6bffb2e4f0cc8ceb4b7da1
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-03-22 15:36:38 +0100

    Enable local and global element paths when building images

--- deploy-image/build-image.sh
+++ deploy-image/build-image.sh
@@ -4,7 +4,7 @@ set -Eeoux pipefail
 
 export DIB_RELEASE=buster
 export DIB_APT_MINIMAL_CREATE_INTERFACES=0
-export DEPLOY_IMAGE_DIR=$(readlink -f $(dirname "$0"))
+
 IMAGE_SHA_TAG=$(git rev-parse HEAD)
 
 docker login -u mvgijssel -p "${REGISTRY_GITHUB_TOKEN}" docker.pkg.github.com
@@ -12,8 +12,9 @@ docker login -u mvgijssel -p "${REGISTRY_GITHUB_TOKEN}" docker.pkg.github.com
 docker run \
        --rm \
        --privileged \
-       -v "${DEPLOY_IMAGE_DIR}/images:/app/images" \
-       -v "${DEPLOY_IMAGE_DIR}/elements:/app/elements" \
+       -v "${SETUP_DEPLOY_IMAGE_DIR}/images:/app/images" \
+       -v "${SETUP_DEPLOY_IMAGE_DIR}/elements:/app/local_elements" \
+       -v "${SETUP_ELEMENTS_DIR}:/app/global_elements" \
        --env DIB_RELEASE \
        --env DIB_APT_MINIMAL_CREATE_INTERFACES \
        "${IMAGE_BUILDER_IMAGE}:${IMAGE_SHA_TAG}" \
--- kubernetes/build-image.sh
+++ kubernetes/build-image.sh
@@ -6,7 +6,7 @@ export DIB_RELEASE=buster
 export DIB_APT_MINIMAL_CREATE_INTERFACES=0
 # Force extlinux instead of grub2
 export DIB_EXTLINUX=1
-export KUBERNETES_DIR=$(readlink -f $(dirname "$0"))
+
 IMAGE_SHA_TAG=$(git rev-parse HEAD)
 
 docker login -u mvgijssel -p "${REGISTRY_GITHUB_TOKEN}" docker.pkg.github.com
@@ -14,8 +14,9 @@ docker login -u mvgijssel -p "${REGISTRY_GITHUB_TOKEN}" docker.pkg.github.com
 docker run \
        --rm \
        --privileged \
-       -v "${KUBERNETES_DIR}/images:/app/images" \
-       -v "${KUBERNETES_DIR}/elements:/app/elements" \
+       -v "${SETUP_KUBERNETES_DIR}/images:/app/images" \
+       -v "${SETUP_KUBERNETES_DIR}/elements:/app/local_elements" \
+       -v "${SETUP_ELEMENTS_DIR}:/app/global_elements" \
        --env DIB_RELEASE \
        --env DIB_APT_MINIMAL_CREATE_INTERFACES \
        --env DIB_EXTLINUX \
