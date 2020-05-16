817c20a38550a555ad3262dc1a36946525998537
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-05-16 09:02:52 +0200

    Script for building pxe Vagrant box

---
+++ razor-server/build-pxe-box.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -Eeoux pipefail
+
+PXE_IMAGE_DIR="${SETUP_RAZOR_SERVER_DIR}/images/pxe"
+PXE_QCOW="${PXE_IMAGE_DIR}/pxe.qcow2"
+
+mkdir -p "${PXE_IMAGE_DIR}"
+
+qemu-img create -f qcow2 "${PXE_QCOW}" 32G
+
+qcow_to_vagrant.sh "${PXE_QCOW}" pxe.box false
