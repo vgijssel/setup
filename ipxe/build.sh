93820bfead9eacaa196e811c288c779cd3269ab9
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-05-16 07:34:01 +0200

    Working pxe provisioning through razor with iPXE Floppy

--- ipxe/build.sh
+++ ipxe/build.sh
@@ -9,4 +9,6 @@ docker_build.sh $IPXE_IMAGE .
 id=$(docker create "${IPXE_IMAGE}:${IMAGE_SHA_TAG}")
 
 docker cp $id:/app/ipxe/src/bin/undionly.kpxe undionly.kpxe
+docker cp $id:/app/ipxe/src/bin/ipxe.dsk ipxe.ds
+
 docker rm -v $id
