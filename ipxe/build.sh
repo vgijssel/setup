bf170e4ae30d06d85be08403f70f5c20f5fb1938
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-05-17 20:34:59 +0200

    Created ipxe build and let razor server build depend on it

--- ipxe/build.sh
+++ ipxe/build.sh
@@ -4,11 +4,11 @@ set -Eeoux pipefail
 
 IMAGE_SHA_TAG=$(git rev-parse HEAD)
 
-docker_build.sh $IPXE_IMAGE .
+docker_build.sh "${IPXE_IMAGE}" "${SETUP_IPXE_DIR}"
 
 id=$(docker create "${IPXE_IMAGE}:${IMAGE_SHA_TAG}")
 
-docker cp $id:/app/ipxe/src/bin/undionly.kpxe undionly.kpxe
-docker cp $id:/app/ipxe/src/bin/ipxe.dsk ipxe.dsk
+docker cp $id:/app/ipxe/src/bin/undionly.kpxe "${SETUP_IPXE_DIR}"/output/undionly.kpxe
+docker cp $id:/app/ipxe/src/bin/ipxe.dsk "${SETUP_IPXE_DIR}"/output/ipxe.dsk
 
 docker rm -v $id
