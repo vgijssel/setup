4491fde0471165d206f54854d93a3696e44366ea
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-06-02 22:16:07 +0200

    Deploy libvirt through razor iPXE with cloud-init-url seeded user-data

--- razor-server/razor-register-pxe.sh
+++ razor-server/razor-register-pxe.sh
@@ -16,4 +16,4 @@ MAC="${1}"
 NODE=$(razor register-node --hw-info net0="${MAC}" --installed false | head -n 1 | sed 's/.*\(node[0-9]*\):/\1/')
 
 razor update-node-metadata --node "${NODE}" --key TARGET_DISK --value /dev/sda
-razor update-node-metadata --node "${NODE}" --key DISK_URL --value http://razor-server:8150/svc/repo/libvirt/libvirt_buster.qcow2
+razor update-node-metadata --node "${NODE}" --key DISK_URL --value http://razor-server:8150/svc/repo/deploy-image/libvirt/libvirt_buster.qcow2
