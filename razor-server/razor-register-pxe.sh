d57b25a2654beb7edbcaf4f8626b07e37b386014
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-06-03 08:55:17 +0200

    Add DISK_VMLINUZ, DISK_INITRD_URL and CLOUD_CONFIG_URL as variable metadata to razor

--- razor-server/razor-register-pxe.sh
+++ razor-server/razor-register-pxe.sh
@@ -17,3 +17,6 @@ NODE=$(razor register-node --hw-info net0="${MAC}" --installed false | head -n 1
 
 razor update-node-metadata --node "${NODE}" --key TARGET_DISK --value /dev/sda
 razor update-node-metadata --node "${NODE}" --key DISK_URL --value http://razor-server:8150/svc/repo/deploy-image/libvirt/libvirt_buster.qcow2
+razor update-node-metadata --node "${NODE}" --key DISK_VMLINUZ_URL --value http://razor-server:8150/svc/repo/deploy-image/libvirt/libvirt_buster.vmlinuz
+razor update-node-metadata --node "${NODE}" --key DISK_INITRD_URL --value http://razor-server:8150/svc/repo/deploy-image/libvirt/libvirt_buster.initrd
+razor update-node-metadata --node "${NODE}" --key CLOUD_CONFIG_URL --value http://razor-server:8150/svc/repo/deploy-image/cloud-init/libvirt-user-data.yml
