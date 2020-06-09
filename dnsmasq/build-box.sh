bdf69a0cec8394ebd2e7965d6df23ae33f2b60c8
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-06-09 13:26:15 +0200

    Don't packer build dnsmasq.box

--- dnsmasq/build-box.sh
+++ dnsmasq/build-box.sh
@@ -2,4 +2,4 @@
 
 set -Eeoux pipefail
 
-qcow_to_vagrant.sh "${SETUP_DNSMASQ_DIR}/images/dnsmasq/dnsmasq_buster.qcow2" "${SETUP_DNSMASQ_DIR}/dnsmasq.box" true
+qcow_to_vagrant.sh "${SETUP_DNSMASQ_DIR}/images/dnsmasq/dnsmasq_buster.qcow2" "${SETUP_DNSMASQ_DIR}/dnsmasq.box" false
