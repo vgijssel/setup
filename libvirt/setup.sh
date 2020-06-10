ccb28d8a385f59eced8bfb69a2a191ab22e474cc
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-06-10 23:05:20 +0200

    Create install script for libvirt macos terraform dependencies

---
+++ libvirt/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -Eeoux pipefail
+
+if [ -d "./terraform-provider-libvirt" ]; then
+  pushd ./terraform-provider-libvirt
+  git pull
+  popd
+else
+  git clone https://github.com/dmacvicar/terraform-provider-libvirt.git
+fi
+
+export GO111MODULE=on
+export GOFLAGS=-mod=vendor
+
+pushd terraform-provider-libvirt
+LDFLAGS="-w" make install
+popd
+
+find ~ -name "terraform-provider-libvirt"
+
+cp -v $GOPATH/bin/terraform-provider-libvirt ~/.terraform.d/plugins/darwin_amd64/terraform-provider-libvirt
+
+terraform init
