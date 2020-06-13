42c0964c2587a658c4d1f369dc3ff02848b6acdd
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-06-13 21:18:59 +0200

    Update docs

--- libvirt/setup_semaphoreci.sh
+++ libvirt/setup_semaphoreci.sh
@@ -2,12 +2,12 @@
 
 set -Eeoux pipefail
 
+# Download and unzip latest terraform
+# https://www.terraform.io/downloads.html
 wget -O terraform.zip https://releases.hashicorp.com/terraform/0.12.26/terraform_0.12.26_linux_amd64.zip
-
-# unzip terraform binary as scripts/terraform which is in PATH
 unzip terraform.zip -d scripts
 
-# download the terraform-libvirt extension
+# Download and extract the latest terraform-libvirt extension
 # https://github.com/dmacvicar/terraform-provider-libvirt/releases
 wget -O terraform-provider-libvirt.tar.gz https://github.com/dmacvicar/terraform-provider-libvirt/releases/download/v0.6.2/terraform-provider-libvirt-0.6.2+git.1585292411.8cbe9ad0.Ubuntu_18.04.amd64.tar.gz
 
