09ef7bfd8e46aa06dc353cba8d4c39f34112a5bc
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-06-19 11:24:19 +0200

    Make sure to break with Vagrant when bastion host port is taken

--- scripts/ssh_vagrant_jump.sh
+++ scripts/ssh_vagrant_jump.sh
@@ -9,4 +9,4 @@ PRIVATE_KEY_PATH="${SETUP_RAZOR_SERVER_DIR}/keys/id_rsa"
 DISABLE_HOST_CHECKING="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 ARGS="$@"
 
-ssh -i ${PRIVATE_KEY_PATH} ${DISABLE_HOST_CHECKING} -o ProxyCommand="ssh -q ${DISABLE_HOST_CHECKING} -i ${PRIVATE_KEY_PATH} -l vagrant -p 2222 -W '[%h]:%p' 127.0.0.1" ${ARGS}
+ssh -v -i ${PRIVATE_KEY_PATH} ${DISABLE_HOST_CHECKING} -o ProxyCommand="ssh -v ${DISABLE_HOST_CHECKING} -i ${PRIVATE_KEY_PATH} -l vagrant -p ${VAGRANT_BASTION_HOST_PORT} -W '[%h]:%p' 127.0.0.1" ${ARGS}
