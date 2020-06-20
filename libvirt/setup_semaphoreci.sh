be2df417d3b3f0ff672211d4665f8559dc82e8bf
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-06-20 11:35:02 +0200

    Create bridge and run dnsmasq on semaphoreci

--- libvirt/setup_semaphoreci.sh
+++ libvirt/setup_semaphoreci.sh
@@ -19,3 +19,29 @@ tar -C "${PLUGIN_DIR}" -xf terraform-provider-libvirt.tar.gz
 pushd "${SETUP_LIBVIRT_DIR}"
 terraform init
 popd
+
+LIBVIRT_BRIDGE="kube_network"
+DNS_IP="192.168.3.1"
+
+sudo brctl addbr $LIBVIRT_BRIDGE
+# ip addr add 192.168.0.2/24 brd + dev br0
+# route add default gw 192.168.0.1 dev br0
+
+sudo ip addr add dev $LIBVIRT_BRIDGE $DNS_IP/24
+sudo ip link set dev $LIBVIRT_BRIDGE up
+
+sudo dnsmasq \
+  --interface="${LIBVIRT_BRIDGE}" \
+  --bind-interfaces \
+  --dhcp-range=192.168.3.100,192.168.3.200,255.255.255.0,12h \
+  --dhcp-leasefile="${SETUP_TMP_DIR}"/dnsmasq.leases \
+  --no-resolv \
+  --no-hosts \
+  --dhcp-option=3,"${DNS_IP}" \
+  --dhcp-option=6,"${DNS_IP}" \
+  --log-facility="${SETUP_LOG_DIR}"/dnsmasq.log \
+  --pid-file="${SETUP_TMP_DIR}"/dnsmasq.pid \
+  --log-dhcp \
+  --log-queries \
+  --user="$(id -un)" \
+  --group="$(id -gn)"
