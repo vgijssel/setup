9b6b65d7547830f3a7090131374ccc535614ca22
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-06-28 21:48:40 +0200

    Update terraform with kubernetes worker and master

--- libvirt/main.tf
+++ libvirt/main.tf
@@ -8,12 +8,6 @@ resource "libvirt_pool" "data" {
   path = "/data/vms/storage"
 }
 
-resource "libvirt_cloudinit_disk" "commoninit" {
-  name           = "commoninit.iso"
-  user_data      = data.template_file.user_data.rendered
-  network_config = data.template_file.network_config.rendered
-  pool           = libvirt_pool.data.name
-}
 
 data "template_file" "user_data" {
   template = file("${path.module}/cloud-init/kubernetes-user-data.yml")
@@ -38,33 +32,38 @@ resource "libvirt_network" "kube_network" {
   autostart = true
 }
 
-resource "libvirt_volume" "master" {
-  name   = "master"
+resource "libvirt_volume" "kubernetes" {
+  name   = "kubernetes"
   pool   = libvirt_pool.data.name
   source = local.kubernetes_image
+}
 
-  # # create associated log file because otherwise libvirt will cry :/
-  # provisioner "file" {
-  #   content     = "file-provisioner-needs-some-content"
-  #   destination = "/data/vms/logs/master.log"
+resource "ansible_group" "all" {
+  inventory_group_name = "all"
+  vars = {
+    ansible_user     = "vagrant"
+    master_ip = libvirt_domain.master.network_interface[0].addresses[0]
+    ansible_ssh_private_key_file="/tmp/id_rsa"
+  }
+}
 
-  #   connection {
-  #     type     = "ssh"
-  #     user     = "vagrant"
-  #     password = "vagrant"
-  #     host     = "libvirt"
+resource "libvirt_cloudinit_disk" "master" {
+  name           = "master_cloudinit.iso"
+  user_data      = data.template_file.user_data.rendered
+  network_config = data.template_file.network_config.rendered
+  meta_data = "local-hostname: master"
+  pool           = libvirt_pool.data.name
+}
 
-  #     bastion_host     = "localhost"
-  #     bastion_port     = 2222
-  #     bastion_user     = "vagrant"
-  #     bastion_password = "vagrant"
-  #   }
-  # }
+resource "libvirt_volume" "master" {
+  name           = "master.qcow2"
+  pool           = libvirt_pool.data.name
+  base_volume_id = libvirt_volume.kubernetes.id
 }
 
 resource "libvirt_domain" "master" {
   name       = "master"
-  memory     = "1024"
+  memory     = "1536"
   vcpu       = 1
   qemu_agent = true
   autostart  = true
@@ -73,7 +72,7 @@ resource "libvirt_domain" "master" {
     volume_id = libvirt_volume.master.id
   }
 
-  cloudinit = libvirt_cloudinit_disk.commoninit.id
+  cloudinit = libvirt_cloudinit_disk.master.id
 
   network_interface {
     network_id     = libvirt_network.kube_network.id
@@ -87,4 +86,57 @@ resource "libvirt_domain" "master" {
     # target_type = "serial"
     # source_path = "/data/vms/logs/master.log"
   }
+}
+
+resource "ansible_host" "master" {
+  inventory_hostname = libvirt_domain.master.name
+  groups             = ["master"]
+}
+
+resource "libvirt_cloudinit_disk" "worker" {
+  name           = "worker${count.index}_cloudinit.iso"
+  user_data      = data.template_file.user_data.rendered
+  network_config = data.template_file.network_config.rendered
+  meta_data = "local-hostname: worker${count.index}"
+  pool           = libvirt_pool.data.name
+  count = var.workers_count
+}
+
+resource "libvirt_volume" "worker" {
+  name           = "worker${count.index}.qcow2"
+  pool           = libvirt_pool.data.name
+  base_volume_id = libvirt_volume.kubernetes.id
+  count = var.workers_count
+}
+
+resource "libvirt_domain" "worker" {
+  count = var.workers_count
+  name       = "worker${count.index}"
+  memory     = "1536"
+  vcpu       = 1
+  qemu_agent = true
+  autostart  = true
+
+  disk {
+    volume_id = libvirt_volume.worker[count.index].id
+  }
+
+  cloudinit = libvirt_cloudinit_disk.worker[count.index].id
+
+  network_interface {
+    network_id     = libvirt_network.kube_network.id
+    wait_for_lease = true
+    bridge         = libvirt_network.kube_network.bridge
+  }
+
+  console {
+    type        = "pty"
+    target_port = "0"
+  }
+}
+
+resource "ansible_host" "worker" {
+  inventory_hostname = libvirt_domain.worker[count.index].name
+  groups             = ["worker"]
+  count = var.workers_count
 }
--- libvirt/variables.tf
+++ libvirt/variables.tf
@@ -1,5 +1,6 @@
 variable "qemu_uri" {
   type        = string
+  # https://libvirt.org/uri.html#URI_remote
   description = "Uri to connect to qemu"
 }
 
@@ -9,4 +10,14 @@ variable "setup_kubernetes_dir" {
 
 variable "setup_root_dir" {
   type = string
+}
+
+variable "setup_razor_server_dir" {
+  type = string
+}
+
+variable "workers_count" {
+  type = number
+  default = 1
+  description = "Number of kubernetes workers to create"
 }
