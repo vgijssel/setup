733147662031a6aba9d8cec350d0f23f0e9ee360
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-09-17 20:42:13 +0200

    Working vagrant up for pxe machine automatically registered in digital rebar

--- digital-rebar/elements/digital-rebar/static/drp/full_install.sh
+++ digital-rebar/elements/digital-rebar/static/drp/full_install.sh
@@ -11,4 +11,15 @@ curl -fsSL get.rebar.digital/stable -o install.sh
 bash ./install.sh install
 
 drpcli bootenvs uploadiso sledgehammer
-drpcli prefs set defaultWorkflow discover-base unknownBootEnv discovery defaultBootEnv sledgehammer defaultStage discover
+
+# drpcli prefs set defaultWorkflow discover-base unknownBootEnv ignore defaultBootEnv sledgehammer defaultStage discover
+
+# This makes sure that systemd output is sent to the serial port
+drpcli profiles set global param kernel-console to "console=tty0 console=ttyS0,115200"
+
+# Install Immutable Image Deployment
+drpcli catalog item install image-deploy
+
+# TODO: install discover-new workflow with associated stage and task!
+
+drpcli prefs set defaultWorkflow discover-new unknownBootEnv discovery defaultBootEnv sledgehammer defaultStage discover
---
+++ digital-rebar/register_machine.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -Eeoux pipefail
+
+export MACHINE_NAME="$1"
+MACHINE_MAC="$2"
+
+MACHINE_CONFIG=$(cat <<EOF
+Available: true
+Name: $MACHINE_NAME
+HardwareAddrs:
+  - '$MACHINE_MAC'
+EOF
+)
+
+
+MACHINE_ID=$(drpcli machines list | jq -r 'map(select(.Name == env.MACHINE_NAME)) | first .Uuid')
+
+if [ "$MACHINE_ID" != "null" ]; then
+  drpcli machines destroy "$MACHINE_ID"
+fi
+
+drpcli machines create "$MACHINE_CONFIG"
+
