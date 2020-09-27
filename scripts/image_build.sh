4f7b6d670e1425f007b6846324ca09f7c4445363
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-09-27 14:40:06 +0200

    Created arm64 provisioner image (which does not boot yet)

--- scripts/image_build.sh
+++ scripts/image_build.sh
@@ -39,6 +39,7 @@ export DIB_DEV_USER_USERNAME=devuser
 export DIB_DEV_USER_PWDLESS_SUDO=true
 export DIB_DEV_USER_PASSWORD=devuser
 export DIB_RELEASE="${DIB_RELEASE:-bionic}"
+export SIZE="${SIZE:-}"
 
 # TODO: maybe mount the global volume as read only?
 # TODO: mounting the cache directory in docker volume seems to make docker crash :')
@@ -53,6 +54,7 @@ CONTAINER=$(
     -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
     -v "$SETUP_ROOT_DIR:$SETUP_ROOT_DIR" \
     --env ELEMENTS_PATH \
+    --env DIB_EXTLINUX=0 \
     --env DIB_RELEASE \
     --env DIB_APT_MINIMAL_CREATE_INTERFACES \
     --env DIB_DEV_USER_USERNAME \
@@ -70,7 +72,7 @@ trap cleanup EXIT HUP INT QUIT PIPE TERM
 docker exec \
   $EXTRA_DOCKER_ARGS \
   $CONTAINER \
-  load_env.sh disk-image-create -t qcow2,tgz -x --image-size "${IMAGE_SIZE}" -o "${DISK_IMAGE_DOCKER_PATH}" "${ELEMENTS}"
+  load_env.sh disk-image-create -a "$ARCHITECTURE" -t "$FORMAT" --image-size "$SIZE" -x -o "$DISK_IMAGE_DOCKER_PATH" "$ELEMENTS"
 
 # Store the directory in the cache
 if [[ "${CI_SEMAPHORE}" = true ]]; then
