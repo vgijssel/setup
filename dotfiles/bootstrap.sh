39693ee96ccecac4016603b9a8453f561c1ea2bf
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-10-13 15:05:37 +0200

    Added more debug statements

--- dotfiles/bootstrap.sh
+++ dotfiles/bootstrap.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-set -Eeou pipefail
+set -Eeoux pipefail
 
 DEVELOPMENT_DIRECTORY="$HOME/Development"
 SETUP_DIRECTORY="$DEVELOPMENT_DIRECTORY/setup"
 CHECKOUT_BRANCH="${BRANCH:-master}"
 
+set +x
 echo "Password: "
 read -s PASSWORD
+set -x
 
 if test ! $(which brew); then
   echo "Installing homebrew..."
 
   # Doing a call with sudo before the homebrew installation makes sure that homebrew doesn't ask for a password
+  set +x
   echo "$PASSWORD" | sudo -S whoami
+  set -x
 
   # The empty echo makes sure we don't need to press RETURN when installing homebrew
   echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
@@ -43,7 +47,9 @@ cd "$SETUP_DOTFILES_DIR"
 ansible-galaxy install -r ./requirements.yml
 
 # Run the complete ansible playbook
+set +x
 echo "$PASSWORD" | sudo -S ansible-playbook -i inventory main.yml
+set -x
 
 # Next steps
 echo "Now signin with 1password: 'eval \$(op signin my.1password.com)'"
