da767022d87f5bbe4828c3f4e6921685b6b87336
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-10-14 12:45:53 +0200

    Speedup console boot by changing asdf to git

--- dotfiles/files/zsh/asdf.zsh
+++ dotfiles/files/zsh/asdf.zsh
@@ -1,4 +1,6 @@
-source $(brew --prefix asdf)/asdf.sh
+if [ -e "$HOME/.asdf/asdf.sh" ]; then
+  source $HOME/.asdf/asdf.sh
+fi
 
 # Make sure brew is in front of asdf, otherwise the Tilt gem will have precendence
 # over the Tilt homebrew package.
