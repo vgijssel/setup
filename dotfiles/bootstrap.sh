fb5a9061dffe85a062ce4d6efa13fe6d9b9a50e9
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-10-15 20:54:03 +0200

    Skip slow steps for now

--- dotfiles/bootstrap.sh
+++ dotfiles/bootstrap.sh
@@ -62,7 +62,7 @@ ansible-galaxy install -r ./requirements.yml
 # We're disabling mas here, because that requires being signed in to the Apple Store
 # Which is not possible on the CI.
 if [[ "$CI" = true ]]; then
-  EXTRA_ANSIBLE_ARGS="--skip-tags ci"
+  EXTRA_ANSIBLE_ARGS="--skip-tags ci,homebrew"
 fi
 
 # Run the complete ansible playbook
