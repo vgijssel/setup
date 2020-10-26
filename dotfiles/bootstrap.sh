644806f9354e0e5f81fd37e9ca6f73e286245e42
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-10-26 11:10:30 +0100

    Create bootstrap script for easy machine installation (#57)
    * Create bootstrap script for easy machine installation
    
    * Remove galaxy roles for asdf and homebrew
    
    * Install asdf and configure golang
    
    * Use rcm to handle dotfiles
    
    * Clone repositories using myrepos
    
    * Disable hidden file search as dotfiles don't have a leading "." now
    
    * Partially restore Alfred workflows
    
    * Updated README
    
    * Fix broken font-source-code-pro-for-powerline installation by installing svn
    
    * Allow virtualbox installation to fail
    
    * Set the user shell to the proper ZSH
    
    * Fix permissions for ZSH
    
    * Set global versions for asdf
    
    * Load asdf syncrhonously
    
    * Don't hardcode username in playbook
    
    * Add build flags to mailcatcher
    
    * Add GitHub to authorised keys
    
    * Updated repos in myrepos config
    
    * Make myrepos use HTTPS for now
    
    * Add docker and fixes
    
    * Added Chrome and iTerm open in new window commands to alfred
    
    * Speedup console boot by changing asdf to git
    
    * Remove gpg-suite and install pinentry-mac separately
    
    * Add spotify
    
    * Set correct hotkey for Alfred
    
    * Added macos customisation script
    
    * Created semaphore ci config
    
    * Install telegram and whatsapp through brew instead of mas
    
    * Install Terraform using asdf
    
    * Add packer and vagrant
    
    * Install dns-heaven
    
    * Created specs for testing bash, sh and zsh environments
    
    * Migrate scripts from zsh to shell_snippets
    
    * Install postgres through asdf

---
+++ dotfiles/bootstrap.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+set -Eeoux pipefail
+DEVELOPMENT_DIR="$HOME/Development"
+SETUP_DIR="$DEVELOPMENT_DIR/setup"
+CHECKOUT_BRANCH="${BRANCH:-master}"
+EXTRA_ANSIBLE_ARGS=''
+
+set +x
+echo "Password: "
+read -s PASSWORD
+
+# Immediately check if the password given is correct
+echo "$PASSWORD" | sudo -S whoami
+set -x
+
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+
+  # Doing a call with sudo before the homebrew installation makes sure that homebrew doesn't ask for a password
+  set +x
+  echo "$PASSWORD" | sudo -S whoami
+  set -x
+
+  # The empty echo makes sure we don't need to press RETURN when installing homebrew
+  echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+else
+    echo "Homebrew found. Skipping installation"
+fi
+
+# Install git and ansible which we use to install the rest
+brew install git ansible || true
+
+if [[ ! -e "$SETUP_DIR" ]]; then
+  echo "Setup directory not found, cloning."
+  git clone -b "$CHECKOUT_BRANCH" https://github.com/mvgijssel/setup.git "$SETUP_DIR"
+
+else
+  echo "Setup directory found '$SETUP_DIR'. Pulling instead"
+
+  cd "$SETUP_DIR"
+
+  git pull origin "$CHECKOUT_BRANCH"
+fi
+
+cd "$SETUP_DIR"
+
+# Load the environment
+source "$SETUP_DIR/.envrc"
+
+# Navigate to the dotfiles directory
+cd "$SETUP_DOTFILES_DIR"
+
+# Ensure that the ansible tmp directory is readable by removing it
+set +x
+echo "$PASSWORD" | sudo -S rm -vrf ~/.ansible/tmp
+set -x
+
+# Install ansible dependencies
+ansible-galaxy install -r ./requirements.yml
+
+# We're disabling mas here, because that requires being signed in to the Apple Store
+# Which is not possible on the CI.
+if [[ "$CI" = true ]]; then
+  EXTRA_ANSIBLE_ARGS="--skip-tags ci,homebrew"
+fi
+
+# Run the complete ansible playbook
+set +x
+ansible-playbook -i inventory $EXTRA_ANSIBLE_ARGS --extra-vars "ansible_sudo_pass=$PASSWORD" main.yml
+set -x
+
+# Load env for the current shell
+source "$HOME/.bash_profile"
+
+# Next steps
+echo "Sync vault secrets using: 'op_sync sync'"
---
+++ dotfiles/macos_customisations.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+set -Eeoux pipefail
+
+# Copied from
+# https://www.going-flying.com/blog/macos-automatic-customization-script.html#
+# https://gist.github.com/mbinna/2357277
+# https://gist.github.com/vraravam/5e28ca1720c9dddacdc0e6db61e093fe
+
+# TODO: Enable "Reduce Motion" in Accessibility > Display
+# TODO: Enable "Use scroll gesture with modified keys to zoom" in Accessibility > Zoom
+# defaults write com.apple.universalaccess closeViewScrollWheelToggle -int 1
+# TODO: Enable "Night Shift" in Displays > Night Shift
+# TODO: Map Caps Lock to Escape for "Apple Internal Keyboard / Trackpad" in Keyboard > Modified Keys
+# TODO: Set "Secondary click" to "Click in bottom right corner" in Trackpad
+
+# Disable "Correct spelling automatically"
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -int 0
+
+# Disable "Capitalize words automatically"
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -int 0
+
+# Disable "Add period with double-space"
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -int 0
+
+# Set "Key Repeat" to be really fast. "2" is the lowest from the Macos UI
+defaults write NSGlobalDomain KeyRepeat -int 1
+
+# Set "Delay Until Repeat" to be faster
+defaults write NSGlobalDomain InitialKeyRepeat -int 12
+
+# https://apple.stackexchange.com/a/298826
+# Enable "Automatically hide and show the Docker"
+defaults write com.apple.dock autohide -bool true
+
+# Set a 1000 second delay before the dock appears
+defaults write com.apple.dock autohide-delay -float 1000
+
+# Disable "Animate opening applications"
+defaults write com.apple.dock no-bouncing -bool TRUE
+
+# Check for updates automatically, daily, and auto-install security updates
+defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
+defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1
+
+# Prevent .DS_Store from being written on network drives
+defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+
+# Prevent .DS_Store from being created on USB Drives
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Enable "Automatically rearrange Spaces based on most recent use" in Mission Control
+defaults write com.apple.dock mru-spaces -bool false
+
+# Set the time format to 24 instead of AM/PM
+defaults write com.apple.menuextra.clock DateFormat -string "EEE d MMM HH:mm"
+
+# Restart the necessary services
+killall Dock
+killall Finder
+killall SystemUIServer # for the clock
