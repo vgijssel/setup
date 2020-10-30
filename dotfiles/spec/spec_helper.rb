7428aa861a533d07ac2292203a52e278a86dbc85
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-10-30 16:37:13 +0100

    Use brewfile (#59)
    * Use Brewfile instead of Ansible to install homebrew packages
    
    * Create GitHub job for dotfiles
    
    * Update homebrew before running brew bundle to prevent error
    
    * Set the correct GIT_BRANCH
    
    * Disable installing App Store dependencies on the CI
    
    * Install sem cli by uninstall parallel
    
    * Install Figma
    
    * Update to how GitHub CI loads environment settings
    
    * Install postgres 12.4
    
    * Source the right token file
    
    * Remove unused mas ansible plugin
    
    * Force RSpec colors in CI
    
    * Add comment for prettier ruby
    
    * Ensure the correct launchctl maxfiles are set
    
    * Remove semaphore
    
    * Cancel previous running workflow

--- dotfiles/spec/spec_helper.rb
+++ dotfiles/spec/spec_helper.rb
@@ -1,5 +1,13 @@
 require 'serverspec'
 require 'pry'
 
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+end
+
 set :backend, :exec
 
