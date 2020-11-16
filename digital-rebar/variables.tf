4ad80309c80c11ab4e4d2eac82816c56dc3c7e41
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-11-16 19:54:02 +0100

    Remove unnecessary files digital rebar

--- digital-rebar/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "setup_box_dir" {
-  type        = string
-  description = "Directory which stores box files"
-}
-
-variable "local_network_bridge_interface" {
-  type        = string
-  description = "Name of the local bridge interface to connect vm to"
-}
