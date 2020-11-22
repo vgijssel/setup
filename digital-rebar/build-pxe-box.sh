49c2974c690dd038b1e91ccdec59a314cc90f8ba
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2020-11-22 17:37:10 +0100

    Make the pxe vagrant box work with drp

--- digital-rebar/build-pxe-box.sh
+++ digital-rebar/build-pxe-box.sh
@@ -4,9 +4,12 @@ set -Eeoux pipefail
 
 PXE_IMAGE_DIR="$SETUP_IMAGE_DIR"
 PXE_QCOW="$PXE_IMAGE_DIR/pxe.qcow2"
+BOX_FILE="$SETUP_BOX_DIR/pxe.box"
 
 mkdir -p "$PXE_IMAGE_DIR"
 
 qemu-img create -f qcow2 "$PXE_QCOW" 32G
 
-qcow_to_vagrant.sh "$PXE_QCOW" "$SETUP_BOX_DIR/pxe.box" false
+qcow_to_vagrant.sh "$PXE_QCOW" "$BOX_FILE" false
+
+vagrant box remove -f "file://$BOX_FILE"
