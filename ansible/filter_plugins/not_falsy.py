08f445faafb8e24fa90e396680cc0f759152f39c
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2021-02-23 17:28:01 +0100

    Speed-up spacemacs, open spacemacs in a new window and others (#60)
    * Install Alfred Emoji, speed-up spacemacs and other fixes
    
    * Remove Alfred Emoji from preferences
    
    * Add not_falsy filter
    
    * Setup ansible filter directory

---
+++ ansible/filter_plugins/not_falsy.py
@@ -0,0 +1,10 @@
+# Copied from https://github.com/ansible/ansible/issues/17329#issuecomment-731281767
+class FilterModule(object):
+    def filters(self):
+        return {'not_falsy': self.not_falsy}
+
+    def not_falsy(self, input):
+        if not input:
+            raise Exception('"Falsy" values not allowed.')
+
+        return input
