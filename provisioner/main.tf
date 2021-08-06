6bf094f45b0ce15c5d124d5d687bbd466fb3be89
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2021-08-06 13:12:10 +0200

    Working multiple output modules in Terraform wd!

--- provisioner/main.tf
+++ provisioner/main.tf
@@ -15,12 +15,10 @@ locals {
   box_path = "{{ data[':box'].tf_file_location }}"
 }
 
-
-
-# module "vagrant" {
-#   source           = "vagrant.tf_file_location"
-#   box_path         = local.box_path
-#   vagrantfile_path = "Vagrantfile.tf_file_location"
-# }
+module "vagrant" {
+  source           = "{{ data[':vagrant'].tf_file_location }}"
+  box_path         = local.box_path
+  vagrantfile_path = "{{ Vagrantfile.tf_file_location }}"
+}
 
 # TODO: provision docker on the vagrant box created (or hostname for pi)
