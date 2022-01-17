f941de103ff0408fa32ea53d27d77f20b3418c07
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2022-01-17 12:42:57 +0100

    Setup RClone backup using Kubernetes cronjob

--- infrastructure/__main__.py
+++ infrastructure/__main__.py
@@ -4,3 +4,10 @@ import radarr
 import sonarr
 import bazarr
 import plex
+import rclone
+
+# import debugpy
+# debugpy.listen(("0.0.0.0", 5678))
+# pulumi.info("debugpy is listening, attach by pressing F5 or ►")
+# debugpy.wait_for_client()
+# pulumi.info("Attached to debugpy!")
