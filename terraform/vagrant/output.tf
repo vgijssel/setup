ac0e3c309a330f175bb9ef24c689ecb1e58170c8
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2022-02-05 12:01:19 +0100

    Cleanup part 3

--- terraform/vagrant/output.tf
+++
@@ -1,9 +0,0 @@
-output "ssh_config" {
-  value       = vagrant_vm.server.ssh_config[0]
-  description = "The Vagrant box SSH config"
-}
-
-output "name" {
-  value       = vagrant_vm.server.machine_names[0]
-  description = "The name of the Vagrant box"
-}
