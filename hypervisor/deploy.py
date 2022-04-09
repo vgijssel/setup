62a93cfb76cfcc4ae7bbd4f42765c9ce76571c29
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2022-04-09 22:06:45 +0200

    Install docker

--- hypervisor/deploy.py
+++ hypervisor/deploy.py
@@ -1,7 +1,51 @@
 from pyinfra.operations import apt, server, files
-# from pyinfra_docker import deploy_docker
 from pyinfra import host
-from pyinfra.facts.server import Arch
+from pyinfra.facts.server import Arch, LsbRelease
+from pyinfra.api.deploy import deploy
+
+
+@deploy('Deploy Docker')
+def deploy_docker(state=None, host=None):
+    '''
+    Install Docker on the target machine.
+    '''
+
+    apt.packages(
+        name='Install apt requirements to use HTTPS',
+        packages=['apt-transport-https', 'ca-certificates'],
+        update=True,
+        cache_time=3600,
+        state=state,
+        host=host,
+    )
+
+    lsb_release = host.get_fact(LsbRelease)
+    lsb_id = lsb_release['id'].lower()
+
+    apt.key(
+        name='Download the Docker apt key',
+        src='https://download.docker.com/linux/{0}/gpg'.format(lsb_id),
+        state=state,
+        host=host,
+    )
+
+    add_apt_repo = apt.repo(
+        name='Add the Docker apt repo',
+        src=(
+            'deb [arch=amd64] https://download.docker.com/linux/{0} {1} stable'
+        ).format(lsb_id, lsb_release['codename']),
+        filename='docker-ce-stable',
+        state=state,
+        host=host,
+    )
+
+    apt.packages(
+        name='Install Docker via apt',
+        packages='docker-ce',
+        update=add_apt_repo.changed,  # update if we added the repo
+        state=state,
+        host=host,
+    )
 
 server.sysctl(
     name='Enable ip4 forwarding',
@@ -15,7 +59,7 @@ apt.packages(
     packages=["mount", "tar", "e2fsprogs", "binutils", "dmsetup", "openssh-client", "git", "cpu-checker", "uuid-runtime"],
 )
 
-# deploy_docker()
+deploy_docker()
 
 # Installation instructions from https://github.com/weaveworks/ignite/blob/main/docs/installation.md
 cni_version = 'v0.9.1'
