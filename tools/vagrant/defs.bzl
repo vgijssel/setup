29d391a6e5ad38f9aa61e78da1eebbfcfb82a1ff
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2022-04-14 19:37:35 +0200

    Store the qcow image sha in the vagrant box info meta data

--- tools/vagrant/defs.bzl
+++ tools/vagrant/defs.bzl
@@ -1,17 +1,18 @@
 load("@bazel_skylib//rules:copy_file.bzl", "copy_file")
 load("//tools/bazel:defs.bzl", "runner_binary")
 
-def qcow_to_vagrant_box(name, src):
+def qcow_to_vagrant_box(name, src, src_sha):
     native.genrule(
         name = name,
         outs = [
             "{}.box".format(name),
         ],
         cmd = """
-        $(execpath //tools/vagrant:qcow_to_vagrant_box) $(execpath //tools/vagrant:box.ovf) $(execpath {src}) $(OUTS)
-        """.format(src = src),
+        $(execpath //tools/vagrant:qcow_to_vagrant_box) $(execpath //tools/vagrant:box.ovf) $(execpath {src}) $(OUTS) $$(cat $(execpath {src_sha}))
+        """.format(src = src, src_sha = src_sha),
         srcs = [
             src,
+            src_sha,
             "//tools/vagrant:box.ovf",
         ],
         tools = [
--- tools/vagrant/qcow_to_vagrant_box.sh
+++ tools/vagrant/qcow_to_vagrant_box.sh
@@ -11,9 +11,13 @@ IMAGE_FILE="$2"
 # name of the output .box file
 VAGRANT_FILE="$3"
 
+# sha of the image
+IMAGE_SHA="$4"
+
 NEW_QCOW_FILE="image.qcow2"
 BOX_VMDK_FILE="box-disk001.vmdk"
 BOX_METADATA_FILE="metadata.json"
+BOX_INFO_FILE="info.json"
 BOX_OVF_FILE="box.ovf"
 
 # copy box.ovf to the current directory so the tar archive
@@ -25,8 +29,18 @@ qemu-img convert -f qcow2 -O vmdk $IMAGE_FILE $BOX_VMDK_FILE
 
 # Create Vagrantfile metadata
 cat <<EOF | tee $BOX_METADATA_FILE
-{"provider":"virtualbox"}
+{
+  "provider": "virtualbox"
+}
+EOF
+
+# Add additional info to the vagrant box 
+# https://www.vagrantup.com/docs/boxes/format
+cat <<EOF | tee $BOX_INFO_FILE
+{
+  "image_sha": "$IMAGE_SHA"
+}
 EOF
 
 # Create a compressed tar with the specified files
-tar zcvf "$VAGRANT_FILE" "$BOX_METADATA_FILE" "$BOX_OVF_FILE" "$BOX_VMDK_FILE"
+tar cvf "$VAGRANT_FILE" "$BOX_METADATA_FILE" "$BOX_INFO_FILE" "$BOX_OVF_FILE" "$BOX_VMDK_FILE"
