dbf7c2cc4f6b334c95dd86be27d0d6eeac7b706e
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2022-04-20 09:23:22 +0200

    Working copying file to remote machine using Pulumi!

--- infrastructure/__main__.py
+++ infrastructure/__main__.py
@@ -16,14 +16,21 @@ import os
 from rules_python.python.runfiles import runfiles
 import vagrant
 import pulumi
+from pulumi_command import remote
 
 r = runfiles.Create()
 vagrantfile_dir = os.path.join(r.Rlocation("setup/hypervisor/.kitchen"), "kitchen-vagrant", "default-ubuntu-focal")
+kerk_txt = r.Rlocation("setup/infrastructure/kerk.txt")
 
 v = vagrant.Vagrant(vagrantfile_dir, quiet_stdout=False, quiet_stderr=False)
-print(v.status())
-print(v.ssh_config())
-print(v.keyfile())
+pulumi.info(v.keyfile())
 
+private_key = open(v.keyfile(), 'r').read()
+connection = remote.ConnectionArgs(
+    host=v.hostname(),
+    port=int(v.port()),
+    private_key=private_key,
+    user=v.user(),
+)
 
-
+remote.CopyFile("kerk", connection=connection, local_path=kerk_txt, remote_path="/tmp/kerk.txt")
