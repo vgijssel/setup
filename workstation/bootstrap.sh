e537385a753041b4961de404e02b2818a8cb251d
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2022-07-22 13:01:00 +0200

    Setup entire skeleton for bootstrap script

--- workstation/bootstrap.sh
+++ workstation/bootstrap.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-# Steps:
-# 1. Install homebrew if does not yet exists
-# 2. Install bazelisk
-# 3. Run bazel //workstation:provision
-#
-# TODOs:
-# - how to have a long running sudo session? make the sudo not expire?
-# From https://apple.stackexchange.com/questions/23494/what-option-should-i-give-the-sudo-command-to-have-the-password-asked-through-a/23514#23514
-# 1. Ask for sudo password using macOS prompt
-# 2. Store the answer in a temporary file 
-#    - setup a descructor to remove the file upon script exit (success or failure)
-# 3. chmod +x the file to make it executable
-# 4. set the SUDO_ASKPASS variable and point it to out temporary file
-# 5. install homebrew
-# 6. install bazelisk
-# 7. the rest
-
 set -Eeoux pipefail
 
 DEVELOPMENT_DIR="$HOME/Development"
@@ -32,6 +15,7 @@ chmod +x "$SUDO_ASKPASS_BINARY"
 trap "rm -f $SUDO_ASKPASS_BINARY" 0
 trap "exit 1" HUP INT QUIT PIPE TERM
 
+# From https://apple.stackexchange.com/questions/23494/what-option-should-i-give-the-sudo-command-to-have-the-password-asked-through-a/23514#23514
 # Prompt user for sudo password
 SUDO_PASSWORD="$(osascript -e 'Tell application "System Events" to display dialog "Sudo password:" default answer "" with hidden answer' -e 'text returned of result' 2>/dev/null)"
 
@@ -42,77 +26,33 @@ echo "$SUDO_PASSWORD"
 EOF
 export SUDO_ASKPASS="$SUDO_ASKPASS_BINARY"
 
-# Check if whoami works now
+# Check if whoami works without prompting for a password
 sudo -A whoami
 
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  # The empty echo makes sure we don't need to press RETURN when installing homebrew
+  echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+else
+    echo "Homebrew found. Skipping installation"
+fi
 
-# set +x
-# echo "Password: "
-# read -s PASSWORD
+if test ! $(which bazelisk); then
+    echo "Installing bazelisk"
+    # TODO: install without updating first
+    brew install bazelisk
+else
+    echo "Bazelisk found. Skipping installation"
+fi
 
-# # TODO: make the sudo session not expire?
-# # Immediately check if the password given is correct
-# echo "$PASSWORD" | sudo -S whoami
-# set -x
+if [[ ! -e "$SETUP_DIR" ]]; then
+  echo "Setup directory not found, cloning."
+  git clone -b "$CHECKOUT_BRANCH" https://github.com/mvgijssel/setup.git "$SETUP_DIR"
+else
+  echo "Setup directory found '$SETUP_DIR'. Pulling instead"
+  cd "$SETUP_DIR"
+  git pull origin "$CHECKOUT_BRANCH"
+fi
 
-# if test ! $(which brew); then
-#   echo "Installing homebrew..."
-
-#   # Doing a call with sudo before the homebrew installation makes sure that homebrew doesn't ask for a password
-#   set +x
-#   echo "$PASSWORD" | sudo -S whoami
-#   set -x
-
-#   # The empty echo makes sure we don't need to press RETURN when installing homebrew
-#   echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-# else
-#     echo "Homebrew found. Skipping installation"
-# fi
-
-# # Install git and ansible which we use to install the rest
-# brew install git || true
-# brew install ansible || true
-
-# if [[ ! -e "$SETUP_DIR" ]]; then
-#   echo "Setup directory not found, cloning."
-#   git clone -b "$CHECKOUT_BRANCH" https://github.com/mvgijssel/setup.git "$SETUP_DIR"
-
-# else
-#   echo "Setup directory found '$SETUP_DIR'. Pulling instead"
-
-#   cd "$SETUP_DIR"
-
-#   git pull origin "$CHECKOUT_BRANCH"
-# fi
-
-# cd "$SETUP_DIR"
-
-# # Load the environment
-# source "$SETUP_DIR/.envrc"
-
-# # Navigate to the dotfiles directory
-# cd "$SETUP_DOTFILES_DIR"
-
-# # Ensure that the ansible tmp directory is readable by removing it
-# set +x
-# echo "$PASSWORD" | sudo -S rm -vrf ~/.ansible/tmp
-# set -x
-
-# # Install ansible dependencies
-# ansible-galaxy collection install -r ./requirements.yml
-
-# # Not running homebrew on the CI because that takes too long
-# if [[ "$CI" = true ]]; then
-#   EXTRA_ANSIBLE_ARGS="--skip-tags homebrew"
-# fi
-
-# # Run the complete ansible playbook
-# set +x
-# ansible-playbook -i inventory $EXTRA_ANSIBLE_ARGS --extra-vars "ansible_sudo_pass=$PASSWORD" main.yml
-# set -x
-
-# # Load env for the current shell
-# source "$HOME/.bash_profile"
-
-# # Next steps
-# echo "Sync vault secrets using: 'op_sync sync'"
+echo "Running workstation provisioning"
+bazel run //workstation:provision
