fb7b7bd57109ecf14481ba047237971f131d3f71
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2022-09-11 16:43:55 +0200

    Define schema for the occupancy plugin in configuration

--- occupancy/custom_components/occupancy/__init__.py
+++ occupancy/custom_components/occupancy/__init__.py
@@ -1,6 +1,18 @@
-from homeassistant import core
+import logging
+import voluptuous as vol
+from occupancy.custom_components.occupancy.const import (
+    DOMAIN,
+)
 
+LOGGER = logging.getLogger(__name__)
 
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({})
+})
+
+# - [ ] setup schema which is used within configuration.yml
+# - [ ] each occupancy thing is a device within home assistant re-exposing associated data
+# - [ ] exposes a select sensor for the state machine
 async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
     """Set up the Occupancy component."""
     # @TODO: Add setup code.
