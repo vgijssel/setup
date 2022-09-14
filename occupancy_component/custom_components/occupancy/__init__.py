db634d86238e80597540e1906da845ba828c8e12
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2022-09-14 09:17:05 +0200

    Subscribe to passed in binary sensor

--- occupancy_component/custom_components/occupancy/__init__.py
+++ occupancy_component/custom_components/occupancy/__init__.py
@@ -1,9 +1,18 @@
 import logging
 from homeassistant.helpers.typing import (
     HomeAssistantType,
+    EventType,
+)
+from homeassistant.const import (
+    EVENT_HOMEASSISTANT_STARTED,
+    STATE_OFF,
+    STATE_ON,
+)
+from homeassistant.helpers.event import (
+    async_track_state_change_event,
 )
 
-LOGGER = logging.getLogger(__name__)
+_LOGGER = logging.getLogger(__name__)
 
 from custom_components.occupancy.const import DOMAIN, ATTR_DOORS, ATTR_ENTRY, ATTR_CONTACT_SENSOR, ATTR_MOTION_SENSOR, ATTR_AREAS, ATTR_OCCUPANCY_SENSORS
 
@@ -33,12 +42,8 @@ CONFIG_SCHEMA = vol.Schema({
     })
 }, extra=vol.ALLOW_EXTRA)
 
-# TODO:
-# - expose an entity through this component
-# - expose doors as entities/devices?
-# - expose areas as entities/devices?
 async def async_setup(hass: HomeAssistantType, config: dict) -> bool:
-    component = EntityComponent(LOGGER, DOMAIN, hass)
+    component = EntityComponent(_LOGGER, DOMAIN, hass)
     entities = []
 
     for door_id, door_config in config[DOMAIN]['doors'].items():
@@ -47,27 +52,40 @@ async def async_setup(hass: HomeAssistantType, config: dict) -> bool:
         motion_sensor = door_config.get(ATTR_MOTION_SENSOR)
 
         entities.append(
-            Door(door_id, entry, contact_sensor, motion_sensor)
+            Door(hass, door_id, entry, contact_sensor, motion_sensor)
         )
 
     await component.async_add_entities(entities)
 
     return True
 
-
-# - open/closed
-# - current_activity
-# - recent_activity 
-# class Door():
-
+# TODO:
+# - Subscribe to the passed in sensor and motion sensor and generate derived state for the door entity
+# - Create area entities represented by a select_input
+# - Subscribe to the door entities by the areas and generate state according to the state machine
+# - Subscribe to the occupancy sensors and update the state machine accordingly
+# - Setup device_class for the door entity sensor
+# - Should we move the Door sensor to the sensor.py file according to the documentation?
+#
+#
+# Door is exposes as a sensor, with simply on/off behaviour
+# on means there is activity at the door
+# active means either
+# - door opened from closed state for a duration of (timeout) seconds
+# - associated motion sensor is triggering
+# change the icon based if the contact sensor is open or closed
+# change the color based if there is activity or not (yellow vs blue)
 class Door(SwitchEntity, RestoreEntity):
     """Representation of a Adaptive Lighting switch."""
 
-    def __init__(self, door_id: str, entry: bool, contact_sensor:str = None, motion_sensor:str = None):
-        """Initialize the Adaptive Lighting switch."""
+    def __init__(self, hass, door_id: str, entry: bool, contact_sensor:str = None, motion_sensor:str = None):
+        self._hass = hass
         self._door_id = ENTITY_ID_FORMAT.format(door_id)
+        self._entry = entry
+        self._contact_sensor = contact_sensor
+        self._motion_sensor = motion_sensor
         self._state = False
-
+        self._remove_listeners = []
 
     @property
     def name(self):
@@ -85,17 +103,6 @@ class Door(SwitchEntity, RestoreEntity):
         """Return true if adaptive lighting is on."""
         return self._state
 
-    # async def async_added_to_hass(self) -> None:
-    #     """Call when entity about to be added to hass."""
-    #     last_state = await self.async_get_last_state()
-    #     _LOGGER.debug("%s: last state is %s", self._name, last_state)
-    #     if (last_state is None and self._initial_state) or (
-    #         last_state is not None and last_state.state == STATE_ON
-    #     ):
-    #         await self.async_turn_on()
-    #     else:
-    #         await self.async_turn_off()
-
     # async def async_turn_on(self, **kwargs) -> None:
     #     """Turn on adaptive lighting sleep mode."""
     #     self._state = True
@@ -103,3 +110,70 @@ class Door(SwitchEntity, RestoreEntity):
     # async def async_turn_off(self, **kwargs) -> None:
     #     """Turn off adaptive lighting sleep mode."""
     #     self._state = False
+
+    async def async_added_to_hass(self) -> None:
+        """Call when entity about to be added to hass."""
+        # await super().async_added_to_hass()
+
+        if self._hass.is_running:
+            await self._setup_listeners()
+        else:
+            self._hass.bus.async_listen_once(
+                EVENT_HOMEASSISTANT_STARTED, self._setup_listeners
+            )
+
+        # TODO: restore state from associated entities here as well?
+
+        # last_state = await self.async_get_last_state()
+        # is_new_entry = last_state is None  # newly added to HA
+        # if is_new_entry or last_state.state == STATE_ON:
+        #     await self.async_turn_on(adapt_lights=not self._only_once)
+        # else:
+        #     self._state = False
+        #     assert not self.remove_listeners
+
+    async def async_will_remove_from_hass(self):
+        self._remove_listeners()
+
+    async def _setup_listeners(self, _=None) -> None:
+        _LOGGER.debug("Called '_setup_listeners'")
+
+        remove_contact_sensor = async_track_state_change_event(
+            self._hass,
+            self._contact_sensor,
+            self._contact_sensor_event,
+        )
+
+        self._remove_listeners.append(remove_contact_sensor)
+
+    def _remove_listeners(self) -> None:
+        while self._remove_listeners:
+            remove_listener = self._remove_listeners.pop()
+            remove_listener()
+
+    async def _contact_sensor_event(self, event: EventType):
+        _LOGGER.debug("Called '_contact_sensor_event' with data %s", event.data)
+
+        from_state = event.data['old_state'].state
+        to_state = event.data['new_state'].state
+
+        # Door opened
+        if to_state == STATE_ON:
+            self._is_door_open = True
+            await self._calculate_presence()
+
+        elif to_state == STATE_OFF:
+            self._is_door_open = False
+            await self._calculate_presence()
+        else:
+            pass
+
+    async def _calculate_presence(self):
+        if self._is_door_open:
+            self._state = True
+        else:
+            self._state = False
+
+        # TODO: what exactly is the difference between these two methods?
+        # self.async_schedule_update_ha_state()
+        await self.async_update_ha_state()
