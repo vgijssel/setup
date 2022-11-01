93f77985def8d82f050bddd9e7e05c54f4281ff8
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2022-11-01 10:13:08 +0100

    undo changes

--- esphome_nimble/src/esphome/core/defines.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "esphome/core/macros.h"
-#define ESPHOME_BOARD "esp32dev"
-#define ESPHOME_VARIANT "ESP32"
-#define USE_ARDUINO_VERSION_CODE VERSION_CODE(1, 0, 6)
-#define USE_STATUS_LED
