ede17a207775acb591f7a4c700524cca7d5f21b8
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2022-11-03 21:32:08 +0100

    Working nimble_rssi sensor!

--- esphome_nimble/components/nimble_rssi/nimble_rssi_sensor.cpp
+++ esphome_nimble/components/nimble_rssi/nimble_rssi_sensor.cpp
@@ -16,7 +16,16 @@ namespace esphome
             auto address = advertised_device->getAddress();
             auto naddress = address.getNative();
 
-            return ble_ll_resolv_rpa(naddress, this->irk_);
+            if (ble_ll_resolv_rpa(naddress, this->irk_))
+            {
+                ESP_LOGD(TAG, "Found device %s", advertised_device->toString().c_str());
+                this->publish_state(advertised_device->getRSSI());
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         };
 
     } // namespace nimble_tracker
