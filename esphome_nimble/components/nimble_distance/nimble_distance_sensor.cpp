026b076b746589d06df789d1d0c200c65bc5ed84
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2022-11-06 10:51:57 +0100

    working instantiation of the filter

--- esphome_nimble/components/nimble_distance/nimble_distance_sensor.cpp
+++ esphome_nimble/components/nimble_distance/nimble_distance_sensor.cpp
@@ -1,3 +1,7 @@
+// Defined distance formula using
+// https://medium.com/beingcoders/convert-rssi-value-of-the-ble-bluetooth-low-energy-beacons-to-meters-63259f307283
+// and copied code from
+// https://github.com/ESPresense/ESPresense/blob/master/lib/BleFingerprint/BleFingerprint.cpp
 #include "nimble_distance_sensor.h"
 
 namespace esphome
@@ -5,6 +9,22 @@ namespace esphome
     namespace nimble_distance
     {
         static const char *const TAG = "nimble_distance";
+
+        Filter::Filter(float fcmin, float beta, float dcutoff) : one_euro_{OneEuroFilter<float, unsigned long>(1, fcmin, beta, dcutoff)}
+        {
+        }
+
+        void NimbleDistanceSensor::setup()
+        {
+            this->filter_ = new Filter(ONE_EURO_FCMIN, ONE_EURO_BETA, ONE_EURO_DCUTOFF);
+
+            // OneEuroFilter<float, unsigned long> one_euro_(0.1, 0.1, 0.1, 0.1);
+            // oneEuro { OneEuroFilter<float, unsigned long>(1, fcmin, beta, dcutoff) }
+            // one_euro_
+            // {
+            //     OneEuroFilter<float, unsigned long>(1, fcmin, beta, dcutoff);
+            // }
+        }
 
         bool NimbleDistanceSensor::update_state(NimBLEAdvertisedDevice *advertised_device)
         {
--- esphome_nimble/components/nimble_distance/nimble_distance_sensor.h
+++ esphome_nimble/components/nimble_distance/nimble_distance_sensor.h
@@ -1,5 +1,14 @@
 #pragma once
 
+// For Filter
+#include <cstddef>
+#include "esp_timer.h"
+#include "SoftFilters.h"
+#define ONE_EURO_FCMIN 1e-5f
+#define ONE_EURO_BETA 1e-7f
+#define ONE_EURO_DCUTOFF 1e-5f
+
+// For NimbleDistanceSensor
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/nimble_tracker/nimble_tracker.h"
@@ -8,10 +17,25 @@ namespace esphome
 {
     namespace nimble_distance
     {
-        class NimbleDistanceSensor : public sensor::Sensor, public Component, public nimble_tracker::NimbleDeviceListener
+        class Filter
         {
+        public:
+            Filter(float fcmin, float beta, float dcutoff);
+
+        protected:
+            OneEuroFilter<float, unsigned long> one_euro_;
+        };
+
+        class NimbleDistanceSensor : public sensor::Sensor,
+                                     public Component,
+                                     public nimble_tracker::NimbleDeviceListener
+        {
+        public:
+            void setup() override;
+
         protected:
             bool update_state(NimBLEAdvertisedDevice *advertised_device) override;
+            Filter *filter_;
         };
     } // namespace nimble_distance
 } // namespace esphome
