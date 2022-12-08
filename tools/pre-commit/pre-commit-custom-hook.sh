f192040f01085c6fc97ed40059d40ef7b5c57e8e
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2022-12-08 10:51:58 +0100

    fix hook invocation

--- tools/pre-commit/pre-commit-custom-hook.sh
+++ tools/pre-commit/pre-commit-custom-hook.sh
@@ -2,14 +2,14 @@
 # NOTE: this is a modification of the original pre-commit pre-commit hook which you can generate with:
 # `pre-commit install --hook-type pre-commit`
 
-ARGS=(hook-impl --config=.pre-commit-config.yaml --hook-type=pre-commit)
+ARGS=(hook-impl --config=.pre-commit-config.yaml --hook-type=pre-commit --color=always)
 HERE="/workspaces/setup"
 ARGS+=(--hook-dir "${HERE}" -- "$@")
 DEVCONTAINER="setup-devcontainer"
-COMMAND="aspect run //tools/pre-commit -- --color always ${ARGS[@]}"
+COMMAND="aspect run //tools/pre-commit -- ${ARGS[@]}"
 
 if [ "$( docker container inspect -f '{{.State.Running}}' $DEVCONTAINER )" == "true" ]; then
-    docker exec -t -w $HERE $DEVCONTAINER /bin/bash -l -c $COMMAND
+    docker exec -t -w $HERE $DEVCONTAINER $COMMAND
 else
    echo "${DEVCONTAINER} is not running, skipping pre-commit." 
    exit 0
