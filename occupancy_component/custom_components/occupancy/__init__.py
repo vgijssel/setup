ad7dc075436aa30b4bcdafb5bda06439986dd6b1
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-02-02 15:13:43 +0100

    lame working of binary_sensor icon and translation

--- occupancy_component/custom_components/occupancy/__init__.py
+++ occupancy_component/custom_components/occupancy/__init__.py
@@ -11,6 +11,11 @@ from homeassistant.const import (
 from homeassistant.helpers.event import (
     async_track_state_change_event,
 )
+from homeassistant.components.binary_sensor import (
+    BinarySensorDeviceClass,
+    BinarySensorEntityDescription,
+    BinarySensorEntity,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -24,7 +29,7 @@ from custom_components.occupancy.const import (
     ATTR_OCCUPANCY_SENSORS,
 )
 
-ENTITY_ID_FORMAT = DOMAIN + ".{}"
+# ENTITY_ID_FORMAT = DOMAIN + ".{}"
 
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
@@ -69,7 +74,7 @@ CONFIG_SCHEMA = vol.Schema(
 
 
 async def async_setup(hass: HomeAssistantType, config: dict) -> bool:
-    component = EntityComponent(_LOGGER, DOMAIN, hass)
+    component = EntityComponent(_LOGGER, "binary_sensor", hass)
     entities = []
 
     for door_id, door_config in config[DOMAIN]["doors"].items():
@@ -77,7 +82,20 @@ async def async_setup(hass: HomeAssistantType, config: dict) -> bool:
         contact_sensor = door_config.get(ATTR_CONTACT_SENSOR)
         motion_sensor = door_config.get(ATTR_MOTION_SENSOR)
 
-        entities.append(Door(hass, door_id, entry, contact_sensor, motion_sensor))
+        entities.append(
+            Door(
+                hass=hass,
+                door_id=door_id,
+                entry=entry,
+                contact_sensor=contact_sensor,
+                motion_sensor=motion_sensor,
+                entity_description=BinarySensorEntityDescription(
+                    key="occupancy_door",
+                    name="Door",
+                    device_class=BinarySensorDeviceClass.DOOR,
+                ),
+            )
+        )
 
     await component.async_add_entities(entities)
 
@@ -85,13 +103,21 @@ async def async_setup(hass: HomeAssistantType, config: dict) -> bool:
 
 
 # TODO:
-# - [x] Subscribe to the passed in sensor and generate derived state for the door entity
-# - [ ] Subscribe to the passed in motion sensor and generate derived state for the door entity
-# - [ ] Create area entities represented by a select_input
-# - [ ] Subscribe to the door entities by the areas and generate state according to the state machine
-# - [ ] Subscribe to the occupancy sensors and update the state machine accordingly
+# - [ ] Setup icon when door is open or closes
+# so it looks like that the trick is to call the "async_add_entities" on a EntityComponent with domain of "binary_sensor".
+# is there a generic way we can add entities like this? So we don't have to create a new EntityComponent for each type of entity?
+#
 # - [ ] Setup device_class for the door entity sensor
+# - [ ] When door opens it is active for X seconds
+# - [ ] When door closes it is active for X seconds
+# - [ ] When door is open and motion sensor triggers the door should be active for X seconds
+# - [ ] When door is closed and motion sensor triggers the door not becomes active
+# - [ ] When door is active make icon yellow
+# - [ ] Ability to restore state after restart
 # - [ ] Should we move the Door sensor to the sensor.py file according to the documentation?
+#
+# https://www.home-assistant.io/integrations/binary_sensor/#device-class
+# Example of various device classes icons in `on` and `off` state. The on image in this example has `state_color: true` specified in the Entities card configuration to receive the icon coloring.
 #
 # Door is exposes as a sensor, with simply on/off behaviour
 # on means there is activity at the door
@@ -100,25 +126,39 @@ async def async_setup(hass: HomeAssistantType, config: dict) -> bool:
 # - associated motion sensor is triggering
 # change the icon based if the contact sensor is open or closed
 # change the color based if there is activity or not (yellow vs blue)
-class Door(SwitchEntity, RestoreEntity):
+
+
+class Door(BinarySensorEntity, RestoreEntity):
     """Representation of a Adaptive Lighting switch."""
+
+    entity_description: BinarySensorEntityDescription
 
     def __init__(
         self,
         hass,
         door_id: str,
         entry: bool,
+        entity_description: BinarySensorEntityDescription,
         contact_sensor: str = None,
         motion_sensor: str = None,
     ):
+        self.entity_description = entity_description
+        self._attr_is_on = False
+
         self._hass = hass
-        self._entity_id = ENTITY_ID_FORMAT.format(door_id)
+        # self._entity_id = ENTITY_ID_FORMAT.format(door_id)
+
         self._door_id = door_id
+        self._unique_id = f"{DOMAIN}_{door_id}"
+
         self._entry = entry
         self._contact_sensor = contact_sensor
         self._motion_sensor = motion_sensor
-        self._state = False
         self._remove_listeners = []
+
+    # icons:
+    # door-open
+    # door-closed
 
     @property
     def name(self):
@@ -126,15 +166,35 @@ class Door(SwitchEntity, RestoreEntity):
         return self._door_id
 
     @property
-    def icon(self) -> str:
-        """Icon to use in the frontend, if any."""
-        # return self._icon
-        return None
+    def unique_id(self):
+        """Return the unique ID of entity."""
+        return self._unique_id
 
-    @property
-    def is_on(self) -> bool | None:
-        """Return true if adaptive lighting is on."""
-        return self._state
+    # @property
+    # def device_class(self):
+    #     parent = super().device_class
+    #     _LOGGER.debug(f"the parent device_class is {parent}")
+
+    #     """Return the name of the device if any."""
+    #     return parent
+
+    # @property
+    # def icon(self) -> str:
+    #     parent = super().device_class
+    #     _LOGGER.debug(f"the parent iconn is {parent}")
+
+    #     raise (ValueError("kerk"))
+    #     """Return the name of the device if any."""
+    #     return parent
+
+    #     """Icon to use in the frontend, if any."""
+    #     # return self._icon
+    #     return None
+
+    # @property
+    # def is_on(self) -> bool | None:
+    #     """Return true if adaptive lighting is on."""
+    #     return self._state
 
     # async def async_turn_on(self, **kwargs) -> None:
     #     """Turn on adaptive lighting sleep mode."""
@@ -155,9 +215,7 @@ class Door(SwitchEntity, RestoreEntity):
                 EVENT_HOMEASSISTANT_STARTED, self._setup_listeners
             )
 
-        # TODO: restore state from associated entities here as well?
-
-        # last_state = await self.async_get_last_state()
+        # self._attr_is_on = await self.async_get_last_state()
         # is_new_entry = last_state is None  # newly added to HA
         # if is_new_entry or last_state.state == STATE_ON:
         #     await self.async_turn_on(adapt_lights=not self._only_once)
@@ -203,9 +261,9 @@ class Door(SwitchEntity, RestoreEntity):
 
     async def _calculate_presence(self):
         if self._is_door_open:
-            self._state = True
+            self._attr_is_on = True
         else:
-            self._state = False
+            self._attr_is_on = False
 
         # TODO: what exactly is the difference between these two methods?
         # self.async_schedule_update_ha_state()
