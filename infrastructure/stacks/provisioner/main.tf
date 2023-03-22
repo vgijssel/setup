255c55533c76677e1a5b79c0fcf0855141c9a941
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-03-22 10:23:10 +0100

    Setup proper helm chart

--- infrastructure/stacks/provisioner/main.tf
+++ infrastructure/stacks/provisioner/main.tf
@@ -1,4 +1,7 @@
-resource "helm_release" "example" {
-  name  = "redis"
-  chart = "https://charts.bitnami.com/bitnami/redis-10.7.16.tgz"
+resource "helm_release" "vault" {
+  name       = "vault"
+  repository = "https://helm.releases.hashicorp.com"
+  chart      = "vault"
+  # Version copied from https://github.com/hashicorp/vault-helm/releases
+  version = "0.23.0"
 }
