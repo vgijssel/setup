7faf1c2d117045a0f539d47ff53ccfd6937e1488
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-03-22 20:07:11 +0100

    remove comment

--- infrastructure/stacks/base/main.tf
+++ infrastructure/stacks/base/main.tf
@@ -1,8 +1,3 @@
-# resource "tfe_organization" "home-production" {
-#   name  = "home-production"
-#   email = ""
-# }
-
 resource "tfe_workspace" "provisioner" {
   name           = var.provisioner_workspace_name
   organization   = "home-production"
