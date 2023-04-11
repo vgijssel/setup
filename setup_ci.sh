41cc4abf9ba92a70445e3901e08790b0355ef7a0
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-04-11 11:50:00 +0200

    use sudo

--- setup_ci.sh
+++ setup_ci.sh
@@ -2,13 +2,13 @@
 
 set -Eeou pipefail 
 
-apt-get update \
-    && apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common \
-    && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - \
-    && add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
-    && apt-get update \
-    && apt-get install -y docker-ce-cli \
-    && groupadd docker \
-    && adduser buildbuddy docker \
-    && apt-get clean \
-    && rm -rf /var/lib/apt/lists/*
+sudo apt-get update \
+    && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common \
+    && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - \
+    && sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+    && sudo apt-get update \
+    && sudo apt-get install -y docker-ce-cli \
+    && sudo groupadd docker \
+    && sudo adduser buildbuddy docker \
+    && sudo apt-get clean \
+    && sudo rm -rf /var/lib/apt/lists/*
