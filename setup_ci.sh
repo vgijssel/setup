9ab9b99ca9f6687b1be01afcdacee574cd1cadd5
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-04-11 12:18:51 +0200

    try podman

--- setup_ci.sh
+++ setup_ci.sh
@@ -3,14 +3,19 @@
 set -Eeou pipefail 
 
 # exit 0 if docker already exists?
+cat /etc/os-release
+env
 
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_$(lsb_release -rs)/Release.key \
+  | gpg --dearmor \
+  | sudo tee /etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg > /dev/null
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg]\
+    https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_$(lsb_release -rs)/ /" \
+  | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list > /dev/null
+sudo apt-get update -qq
 sudo apt-get -qq -y install podman
-
-
-echo $PATH
-
-which docker
-which podman
 
 podman image ls || true
 
@@ -18,7 +23,7 @@ docker image ls || true
 
 sudo service docker start
 
-sudo apt-get update \
+source /etc/os-release \
     && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common \
     && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - \
     && sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
