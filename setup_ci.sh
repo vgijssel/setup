41ff152c3285316df9b84d72737ff5f717b7d037
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-04-11 14:41:05 +0200

    install docker

--- setup_ci.sh
+++ setup_ci.sh
@@ -44,7 +44,6 @@ podman version
 # docker image ls || true
 
 source /etc/os-release \
-    && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common \
     && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - \
     && sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
     && sudo apt-get update \
