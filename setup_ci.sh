e8ae38d785c714dec71e9759ff75c1bac9fa7d20
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-04-11 12:35:50 +0200

    better podman

--- setup_ci.sh
+++ setup_ci.sh
@@ -1,38 +1,30 @@
  #!/usr/bin/env bash
 
-set -Eeou pipefail 
+set -Eeoux pipefail 
 
 # exit 0 if docker already exists?
 cat /etc/os-release
 env
 
-sudo apt-get update -qq
-sudo apt-get -qq -y install gnupg
+sudo apt-get -y install gnupg
 
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_$(lsb_release -rs)/Release.key \
-  | gpg --dearmor \
-  | sudo tee /etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg > /dev/null
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg]\
-    https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_$(lsb_release -rs)/ /" \
-  | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list > /dev/null
-sudo apt-get update -qq
-sudo apt-get -qq -y install podman
+source /etc/os-release
+
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key | sudo apt-key add -
+
+sudo apt-get update
+sudo apt-get -y install podman
 
 podman image ls || true
-
-
 docker image ls || true
 
-sudo service docker start
-
-source /etc/os-release \
-    && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common \
-    && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - \
-    && sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
-    && sudo apt-get update \
-    && sudo apt-get install -y docker-ce-cli \
-    && sudo groupadd docker || true \
-    && sudo adduser buildbuddy docker || true
+# source /etc/os-release \
+#     && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common \
+#     && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - \
+#     && sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+#     && sudo apt-get update \
+#     && sudo apt-get install -y docker-ce-cli \
+#     && sudo groupadd docker || true \
+#     && sudo adduser buildbuddy docker || true
 
