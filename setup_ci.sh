791e28b48663f22ce41d795ca773210b3785d4b7
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-04-11 14:55:43 +0200

    fix prompt

--- setup_ci.sh
+++ setup_ci.sh
@@ -44,7 +44,7 @@ sudo apt-get install -y \
     gnupg
 
 sudo mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
