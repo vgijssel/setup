ea33b677bbc9d0211e8146ddb37f68cfe0c18889
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-04-11 14:39:15 +0200

    install docker

--- setup_ci.sh
+++ setup_ci.sh
@@ -31,23 +31,24 @@ update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
 # sudo podman network create podman
 
 # Let's pretend we're docker
-sudo ln -s -f /usr/bin/podman /usr/bin/docker
+# sudo ln -s -f /usr/bin/podman /usr/bin/docker
+rm -f /usr/bin/docker
 
-docker image ls 
-docker info
-docker version
+podman image ls 
+podman info
+podman version
 
 # docker --log-level debug run -it debian:latest ls -la
 
 # podman image ls || true
 # docker image ls || true
 
-# source /etc/os-release \
-#     && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common \
-#     && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - \
-#     && sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
-#     && sudo apt-get update \
-#     && sudo apt-get install -y docker-ce-cli \
-#     && sudo groupadd docker || true \
-#     && sudo adduser buildbuddy docker || true
+source /etc/os-release \
+    && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common \
+    && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - \
+    && sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+    && sudo apt-get update \
+    && sudo apt-get install -y docker-ce-cli
+
+docker version
 
