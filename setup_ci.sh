4cd6447a13ae6846a881e19df623ea7e1abb02aa
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-04-11 11:56:06 +0200

    more debugging

--- setup_ci.sh
+++ setup_ci.sh
@@ -4,6 +4,12 @@ set -Eeou pipefail
 
 # exit 0 if docker already exists?
 
+echo $PATH
+
+which docker
+
+docker image ls
+
 sudo apt-get update \
     && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common \
     && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - \
@@ -13,6 +19,3 @@ sudo apt-get update \
     && sudo groupadd docker || true \
     && sudo adduser buildbuddy docker || true
 
-echo $PATH
-
-which docker
