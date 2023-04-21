c01e7dcf78ac1a480536eb30dc9ec6d897ede858
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-04-21 12:03:24 +0200

    Support running task rule inside genrule

---
+++ rules/rules_task/tests/test_py_binary_with_runfile.py
@@ -0,0 +1,23 @@
+import runfiles
+
+r = runfiles.Create()
+
+
+def _rlocation_to_path(rlocation):
+    p = r.Rlocation(rlocation)
+
+    if not p:
+        raise Exception("Unable to find runfile: {}".format(rlocation))
+
+    return p
+
+
+# get file content
+def _get_file_content(path):
+    with open(path, "r") as file:
+        return file.read()
+
+
+content = _get_file_content(_rlocation_to_path("rules_task/tests/test_file.txt"))
+
+print(f"from python: {content}")
