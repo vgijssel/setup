ec16aa2a3b9566d003fc5279eda50d7da46e0b6c
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-05-03 21:24:30 +0200

    remove acme

--- provisioner/group_data/dev.py
+++ provisioner/group_data/dev.py
@@ -1,4 +1,2 @@
 inside_docker = True
 teleport_public_addr = "127.0.0.1:10443"
-teleport_acme_enabled = "no"
-teleport_acme_email = ""
--- provisioner/group_data/prod.py
+++ provisioner/group_data/prod.py
@@ -1,5 +1,3 @@
 inside_docker = False
 ssh_user = "ubuntu"
 teleport_public_addr = "tele.vgijssel.nl:443"
-teleport_acme_enabled = "yes"
-teleport_acme_email = "haves_borzoi_0o@icloud.com"
