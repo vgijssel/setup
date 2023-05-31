c4e63fa492db18f19a5f83576c62c13fdeb55aae
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-05-31 11:22:18 +0200

    Setup New Relic Agent

--- provisioner/deploys/monitoring/tasks/install_monitoring.py
+++ provisioner/deploys/monitoring/tasks/install_monitoring.py
@@ -2,6 +2,8 @@ from pyinfra.api.deploy import deploy
 from pyinfra.operations import files, server, apt, systemd
 from pyinfra import host
 from provisioner.utils import one_password_item
+from pyinfra.facts.server import LsbRelease
+from pyinfra.facts.deb import DebArch
 
 
 @deploy("Install Monitoring")
@@ -52,6 +54,62 @@ def install_monitoring():
     systemd.service(
         name="Enable the telegraf service",
         service="telegraf.service",
+        running=True,
+        restarted=True,
+        enabled=True,
+        _sudo=True,
+    )
+
+    new_relic_license_key = one_password_item("new_relic_license_key")["password"]
+
+    files.template(
+        name="Copy New Relic config",
+        src="provisioner/deploys/monitoring/files/newrelic-infra.yml.j2",
+        dest="/etc/newrelic-infra.yml",
+        create_remote_dir=True,
+        _sudo=True,
+        user="root",
+        group="root",
+        mode="0644",
+        new_relic_license_key=new_relic_license_key,
+    )
+
+    lsb = host.get_fact(LsbRelease)
+    version_codename = lsb["codename"]
+
+    apt.packages(
+        name="Install apt key requirements",
+        packages=["curl", "gnupg"],
+        _sudo=True,
+    )
+
+    server.shell(
+        name="Add New Relic key",
+        commands=[
+            "curl -fsSL https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/newrelic-infra.gpg",
+        ],
+        _sudo=True,
+    )
+
+    apt.repo(
+        name="Install New Relic repo",
+        src=f"deb https://download.newrelic.com/infrastructure_agent/linux/apt/ {version_codename} main",
+        filename="newrelic-infra",
+        _sudo=True,
+    )
+
+    apt.packages(
+        name="Install New Relic",
+        packages=["newrelic-infra"],
+        update=True,
+        present=True,
+        cache_time=24 * 60 * 60,
+        _sudo=True,
+    )
+
+    systemd.service(
+        name="Enable the New Relic service",
+        service="newrelic-infra.service",
         running=True,
         restarted=True,
         enabled=True,
