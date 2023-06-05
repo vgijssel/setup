c792136850af4c2aea9627e8f49c53b6a2f56032
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-06-05 16:38:59 +0200

    working client to start workflow

--- tools/buildbuddy/client.py
+++ tools/buildbuddy/client.py
@@ -1,72 +1,73 @@
+import pprint
 from tools.buildbuddy.service_grpc import ApiServiceStub
 from tools.buildbuddy.invocation_pb2 import GetInvocationRequest, InvocationSelector
 from tools.buildbuddy.workflow_pb2 import ExecuteWorkflowRequest
+from tools.buildbuddy.log_pb2 import GetLogRequest, LogSelector
 from grpclib.client import Channel
 import asyncio
 import os
+import sys
 
 
-async def start_workflow():
-    async with Channel("app.buildbuddy.io", 1986, ssl=True) as channel:
-        stub = ApiServiceStub(channel)
-        token = os.environ["BUILDBUDDY_TOKEN"]
-        invocation_id = os.environ["BUILDBUDDY_INVOCATION_ID"]
-        reply = await stub.ExecuteWorkflow(
-            ExecuteWorkflowRequest(
-                repo_url="https://github.com/mvgijssel/setup",
-                ref="master",
-                action_names=["Test all targets"],
-            ),
-            metadata={"x-buildbuddy-api-key": token},
-        )
-
-        reply.action_statuses[0].invocation_id
-
-        import pdb
-
-        pdb.set_trace()
-
-        print(reply)
-
-    # reply = await stub.GetInvocation(
-    #     GetInvocationRequest(
-    #         selector=InvocationSelector(invocation_id=invocation_id),
-    #         # include_metadata=True,
-    #     ),
-    #     metadata={"x-buildbuddy-api-key": token},
-    # )
+async def start_workflow(stub):
+    token = os.environ["BUILDBUDDY_TOKEN"]
+    reply = await stub.ExecuteWorkflow(
+        ExecuteWorkflowRequest(
+            repo_url="https://github.com/mvgijssel/setup",
+            ref="master",
+            action_names=["Test all targets"],
+        ),
+        metadata={"x-buildbuddy-api-key": token},
+    )
+    print(reply)
+    return reply.action_statuses[0].invocation_id
 
 
-# duration_usec used to determine if invocation is done or not.
-# success=True only present when invocation is done and successful
+async def get_log(stub, invocation_id):
+    token = os.environ["BUILDBUDDY_TOKEN"]
+    reply = await stub.GetLog(
+        GetLogRequest(selector=LogSelector(invocation_id=invocation_id)),
+        metadata={"x-buildbuddy-api-key": token},
+    )
+    print(reply)
+    return reply
+
+
+async def get_invocation(stub, invocation_id):
+    token = os.environ["BUILDBUDDY_TOKEN"]
+    reply = await stub.GetInvocation(
+        GetInvocationRequest(
+            selector=InvocationSelector(invocation_id=invocation_id),
+        ),
+        metadata={"x-buildbuddy-api-key": token},
+    )
+    print(reply)
+    return reply.invocation[0]
+
+
 async def main():
-    await start_workflow()
-
     async with Channel("app.buildbuddy.io", 1986, ssl=True) as channel:
         stub = ApiServiceStub(channel)
-        token = os.environ["BUILDBUDDY_TOKEN"]
-        invocation_id = os.environ["BUILDBUDDY_INVOCATION_ID"]
-        reply = await stub.GetInvocation(
-            GetInvocationRequest(
-                selector=InvocationSelector(invocation_id=invocation_id),
-                # include_metadata=True,
-            ),
-            metadata={"x-buildbuddy-api-key": token},
-        )
+        invocation_id = await start_workflow(stub)
 
-        import pdb
+        while True:
+            invocation = await get_invocation(stub, invocation_id)
 
-        pdb.set_trace()
+            if invocation.duration_usec > 0:
+                break
 
-        print(reply)
+            print("sleeping 5...")
+            await asyncio.sleep(1)
+
+        log = await get_log(stub, invocation_id)
+
+        print(log)
+
+        if invocation.success:
+            sys.exit(0)
+        else:
+            sys.exit(1)
 
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-# import pdb
-
-# pdb.set_trace()
-
-# TODO: "@go_googleapis//google/rpc:status_proto",
-# this dependency does not work with Python?
