f7f4975039cb432a25fd0cf22ffde2f1a3d3c055
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-09-25 20:55:48 +0200

    Really hacky working python image

--- hello-docker.nix
+++ hello-docker.nix
@@ -16,15 +16,14 @@
 # How do we connect to the remote builder now?
 # We can setup https://github.com/LnL7/nix-docker? As a remote builder machine?
 
+# with import <nixpkgs> { system = "aarch64-linux"; };
+with import <nixpkgs> { system = "aarch64-linux"; };
 
 let
   # pkgs = import <nixpkgs> { system = "x86_64-linux"; };
-  pkgs = import <nixpkgs> { system = "aarch64-linux"; };
+  # pkgs = import <nixpkgs> { system = "aarch64-linux"; };
   # pkgs = (import <nixpkgs> {}).pkgsCross.aarch64-multiplatform;
-
   # scienv = pkgs.python3.withPackages (ps: with ps; [ numpy scipy ]);
-in
-rec {
   jax = pkgs.dockerTools.buildImage {
     name = "hello-docker";
     tag = "latest";
@@ -34,10 +33,28 @@ rec {
         coreutils
         bashInteractive
         moreutils
+        lefthook
+        vim
 
         # scienv
       ];
       pathsToLink = [ "/bin" ];
+      # extraCommands = ''
+      #         mkdir -p root
+      #         mkdir -p usr/bin
+      #         ln -s /bin/env usr/bin/env
+      #         cat <<-"EOF" > "usr/bin/python3"
+      #   #!/bin/sh
+      #   export LD_LIBRARY_PATH="/lib64:/lib"
+      #   exec -a "$0" "/bin/python3" "$@"
+      #   EOF
+      #         chmod +x usr/bin/python3
+      #         ln -s /usr/bin/python3 usr/bin/python
+      # '';
     };
   };
-}
+in
+runCommand "python38-base-image" { } ''
+  mkdir -p $out
+  gunzip -c ${jax} > $out/image
+''
