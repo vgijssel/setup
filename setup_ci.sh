3edb00ae9f1df71215112d9fc342b0319e4b4a55
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-09-26 16:17:33 +0200

    better

--- setup_ci.sh
+++ setup_ci.sh
@@ -3,4 +3,8 @@
 set -x
 set -e
 
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --init none
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux \
+  --init none \
+  --extra-conf "sandbox = false" \
+  --no-start-daemon \
+  --no-confirm
