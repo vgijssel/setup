5d99f27f5c419bd8ffc0bc92c5fa81c9ff3a51e7
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-10-08 11:08:12 +0200

    Try to use docker proxy

--- setup_ci.sh
+++ setup_ci.sh
@@ -37,9 +37,10 @@ set -e
 # sudo ln -f -s $BUILD_WORKSPACE_DIRECTORY/nix-build /usr/local/bin/nix-build
 
 which docker
+docker --version
 
-sudo curl -L https://hydra.nixos.org/job/nix/maintenance-2.14/buildStatic.x86_64-linux/latest/download-by-type/file/binary-dist -o /usr/local/bin/nix
-sudo chmod +x /usr/local/bin/nix
+# sudo curl -L https://hydra.nixos.org/job/nix/maintenance-2.14/buildStatic.x86_64-linux/latest/download-by-type/file/binary-dist -o /usr/local/bin/nix
+# sudo chmod +x /usr/local/bin/nix
 sudo ln -f -s $BUILD_WORKSPACE_DIRECTORY/nix-build /usr/local/bin/nix-build
 
 nix-build --version
