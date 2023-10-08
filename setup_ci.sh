9c69d3a03ddb6fbffd62e7eae82d2560d25a4a59
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-10-08 10:43:26 +0200

    Add debugging

--- setup_ci.sh
+++ setup_ci.sh
@@ -39,3 +39,5 @@ set -e
 sudo curl -L https://hydra.nixos.org/job/nix/maintenance-2.14/buildStatic.x86_64-linux/latest/download-by-type/file/binary-dist -o /usr/local/bin/nix
 sudo chmod +x /usr/local/bin/nix
 sudo ln -f -s $BUILD_WORKSPACE_DIRECTORY/nix-build /usr/local/bin/nix-build
+
+nix-build --version
