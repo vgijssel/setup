9f5269dfd417fdecba610ae03bd84efd88f82522
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-10-08 10:40:57 +0200

    Try something new

--- setup_ci.sh
+++ setup_ci.sh
@@ -5,33 +5,37 @@ set -e
 
 
 
-if [ -f /nix/receipt.json ]; then
-  echo "Uninstall nix as it's already here"
-  sudo /nix/nix-installer uninstall --no-confirm
-else
-  echo "No nix here"
-  # echo "Installing Nix"
+# if [ -f /nix/receipt.json ]; then
+#   echo "Uninstall nix as it's already here"
+#   sudo /nix/nix-installer uninstall --no-confirm
+# else
+#   echo "No nix here"
+#   # echo "Installing Nix"
 
-  # curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux \
-  #   --init none \
-  #   --extra-conf "sandbox = false" \
-  #   --no-start-daemon \
-  #   --no-confirm
-fi
+#   # curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux \
+#   #   --init none \
+#   #   --extra-conf "sandbox = false" \
+#   #   --no-start-daemon \
+#   #   --no-confirm
+# fi
 
-# sudo which nix-build
-env
-echo $PATH
-sudo env
-# ls -la /nix/var/nix/profiles/default/bin/
+# # sudo which nix-build
+# env
+# echo $PATH
+# sudo env
+# # ls -la /nix/var/nix/profiles/default/bin/
 
-# install nix portable
-sudo curl -L https://github.com/DavHau/nix-portable/releases/download/v009/nix-portable -o /usr/local/bin/nix-portable
-sudo chmod +x /usr/local/bin/nix-portable
+# # install nix portable
+# sudo curl -L https://github.com/DavHau/nix-portable/releases/download/v009/nix-portable -o /usr/local/bin/nix-portable
+# sudo chmod +x /usr/local/bin/nix-portable
 
-export NP_DEBUG=2
-export NP_RUNTIME=proot
+# export NP_DEBUG=2
+# export NP_RUNTIME=proot
 
-nix-build --version
+# nix-build --version
 
+# sudo ln -f -s $BUILD_WORKSPACE_DIRECTORY/nix-build /usr/local/bin/nix-build
+
+sudo curl -L https://hydra.nixos.org/job/nix/maintenance-2.14/buildStatic.x86_64-linux/latest/download-by-type/file/binary-dist -o /usr/local/bin/nix
+sudo chmod +x /usr/local/bin/nix
 sudo ln -f -s $BUILD_WORKSPACE_DIRECTORY/nix-build /usr/local/bin/nix-build
