c3dbf246f23a87c86910c3cc36abf84d2fbf4075
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-10-24 11:38:26 +0200

    Add docs

--- rules/rules_task/generate-release-notes.sh
+++ rules/rules_task/generate-release-notes.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
 
-# trigger
+set -Eeou pipefail
 
-set -o errexit -o nounset -o pipefail
+export VERSION="v$1"
 
-echo "hello"
+export PREFIX="rules_task-$VERSION"
+export ARCHIVE="rules_task-$VERSION.tar.gz"
+
+git archive --format=tar --prefix=${PREFIX}/ HEAD | gzip > $ARCHIVE
+
+export SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
+
+cat << EOF
+## Using WORKSPACE:
+
+\`\`\`starlark
+
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+
+http_archive(
+    name = "rules_task",
+    sha256 = "${SHA}",
+    strip_prefix = "${PREFIX}",
+    url = "https://github.com/vgijssel/setup/releases/download/${PREFIX}/${ARCHIVE}",
+)
+EOF
+
+echo "\`\`\`" 
