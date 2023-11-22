124d917370b77ac1442a58e6ae2bd03f7b94ee22
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-11-22 12:28:54 +0100

    Cleanup

--- rules/rules_release/lib/actions/PublishAction.mjs
+++ rules/rules_release/lib/actions/PublishAction.mjs
@@ -1,5 +1,4 @@
 import ReleaseRepository from "../repositories/ReleaseRepository.mjs";
-import ConfigRepository from "../repositories/ConfigRepository.mjs";
 import PublishRepository from "../repositories/PublishRepository.mjs";
 
 export default class PublishAction {
@@ -9,76 +8,18 @@ export default class PublishAction {
   }
 
   async execute() {
-    const configRepository = new ConfigRepository();
     const releaseRepository = new ReleaseRepository(this.configPaths);
     const publishRepository = new PublishRepository();
     const releases = await releaseRepository.getAll();
 
-    // console.log(this.publishCmds);
-    // console.log(releases);
+    for (const release of releases) {
+      for (const publishCmd of release.publish_cmds) {
+        await publishRepository.executeCmd(publishCmd);
+      }
+    }
 
     for (const cmd of this.publishCmds) {
       await publishRepository.executeCmd(cmd);
     }
-
-    // for (const release of releases) {
-    //   for (const publishCmd of release.publish_cmds) {
-    //     await publishRepository.executeCmd(publishCmd);
-    //   }
-    // }
-
-    //     const packageRepository = new PackageRepository({
-    //       packagesDir: configRepository.packagesDir(),
-    //     });
-    //     const versionRepository = new VersionRepository();
-    //     const changesetRepository = new ChangesetRepository({
-    //       workspaceDir: configRepository.workspaceDir(),
-    //       changesetDir: configRepository.changesetDir(),
-    //       changesetBinaryPath: this.changesetsPath,
-    //     });
-    //     const changelogRepository = new ChangelogRepository();
-    //     const releases = await releaseRepository.getAll();
-
-    //     for (const release of releases) {
-    //       const releaseVersion = await versionRepository.getByFile(
-    //         release.version_file
-    //       );
-
-    //       const packageFile = await packageRepository.write({
-    //         name: release.name,
-    //         version: releaseVersion,
-    //         deps: release.deps,
-    //       });
-
-    //       await changelogRepository.write(
-    //         path.dirname(packageFile),
-    //         release.changelog_file
-    //       );
-    //     }
-
-    //     await changesetRepository.updateVersions();
-
-    //     for (const release of releases) {
-    //       const packageData = await packageRepository.getContentByName(
-    //         release.name
-    //       );
-    //       const currentVersion = await versionRepository.getByFile(
-    //         release.version_file
-    //       );
-    //       const newVersion = packageData.version;
-
-    //       await versionRepository.updateByFile(release.version_file, newVersion);
-
-    //       const packageFile = packageRepository.getPathByName(release.name);
-    //       await changelogRepository.copyBackIntoFile(
-    //         path.dirname(packageFile),
-    //         release.changelog_file
-    //       );
-
-    //       console.log(
-    //         `Updated version for release ${release.name} from ${currentVersion} to ${newVersion}`
-    //       );
-    //     }
-    //   }
   }
 }
