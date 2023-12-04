2842cdd13d8925ada8f7a85456814526e4fabcad
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-12-04 15:34:30 +0100

    Make sure the version command works

--- rules/rules_release/examples/workspace/tests/test.sh
+++ rules/rules_release/examples/workspace/tests/test.sh
@@ -5,6 +5,20 @@ export GENERATE=$1
 export VERSION=$2
 export PUBLISH=$3
 
+function convert_to_actual_file() {
+    local file=$1
+    cat $file > $file.tmp
+    rm $file
+    mv $file.tmp $file
+}
+
+# Convert the changelog and version into actual files,
+# because the files referenced by the Bazel symlinks are readonly
+convert_to_actual_file CHANGELOG_foo.md
+convert_to_actual_file VERSION_foo.txt
+convert_to_actual_file CHANGELOG_bar.md
+convert_to_actual_file VERSION_bar.txt
+
 $GENERATE
 $VERSION
 $PUBLISH
