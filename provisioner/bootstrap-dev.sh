524b5b368799b5f86f9ee3aeb41a936efa8d4767
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2023-12-17 21:01:25 +0100

    Working dev provisionen

---
+++ provisioner/bootstrap-dev.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+apt-get update
+apt-get install -y sudo lsb-release
+useradd -m ubuntu -s /bin/bash
+adduser ubuntu sudo
+echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
