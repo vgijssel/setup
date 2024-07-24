665ea2c42416ffbbe995967c8531848ca7ff255a
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2024-07-24 20:07:48 +0200

    Setup SSH config

--- devenv/deploy.py
+++ devenv/deploy.py
@@ -1,8 +1,7 @@
 from deploys.editor.tasks.install_editor import install_editor
+from deploys.ssh.tasks.install_ssh import install_ssh
 from deploys.terminal.tasks.install_terminal import install_terminal
 
-# from deploys.languages.tasks.install_languages import install_languages
-# from deploys.ssh.tasks.install_ssh import install_ssh
 # from deploys.utilities.tasks.install_utilities import install_utilities
 # from deploys.workflow.tasks.install_workflow import install_workflow
 from pyinfra import host
@@ -13,11 +12,9 @@ if host.data.get("install_terminal"):
 if host.data.get("install_editor"):
     install_editor()
 
-# if host.data.get("install_languages"):
-#     install_languages()
-
-# if host.data.get("install_ssh"):
-#     install_ssh()
+# TODO: implement this
+if host.data.get("install_ssh"):
+    install_ssh()
 
 # if host.data.get("install_workflow"):
 #     install_workflow()
--- devenv/deploys/ssh/tasks/install_ssh.py
+++ devenv/deploys/ssh/tasks/install_ssh.py
@@ -1,10 +1,7 @@
+from helpers.home_link import home_link
 from pyinfra import host
 from pyinfra.api.deploy import deploy
-from pyinfra.facts.server import Home
 from pyinfra.operations import brew, files
-
-from workstation.helpers import onepassword
-from workstation.helpers.home_link import home_link
 
 
 @deploy("Install SSH")
@@ -21,29 +18,24 @@ def install_ssh():
     )
 
     ssh_config_files = [
-        "ssh/config",
-        "gitconfig",
-        "config/1Password/ssh/agent.toml",
+        ".ssh/config",
+        ".gitconfig",
+        ".config/1Password/ssh/agent.toml",
     ]
 
     for file in ssh_config_files:
         home_link(
-            source_file=f"setup/workstation/deploys/ssh/files/{file}",
-            target_file=f".{file}",
+            source_file=f"deploys/ssh/files/{file}",
+            target_file=file,
         )
 
-    home_dir = host.get_fact(Home)
-    onepassword.sync(
-        name="Sync 1Password user_config",
-        local_file=f"{home_dir}/.ssh/user_config",
-        remote_file=f"~/.ssh/user_config",
-        vault="Workstation",
-    )
-
-    files.line(
-        name="Ensure macOS is only reachable using SSH from localhost",
-        path="/etc/ssh/sshd_config",
-        line="AllowUsers *@127.0.0.1",
+    files.link(
+        name=f"Ensure macOS is only reachable using SSH from localhost",
+        path="/etc/ssh/sshd_config.d/200-devenv.conf",
+        target="deploys/ssh/files/etc/ssh/sshd_config.d/200-devenv.conf",
         present=True,
-        _sudo=True,
+        symbolic=True,
+        force=True,
+        create_remote_dir=True,
+        # _sudo=True,
     )
