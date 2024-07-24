e26e82dab053126c398f7b7dd818d2d1c4604bb4
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2024-07-24 12:08:50 +0200

    Working skaffold

--- devenv/deploy.py
+++ devenv/deploy.py
@@ -1,13 +1,14 @@
-# from workstation.deploys.terminal.tasks.install_terminal import install_terminal
-# from workstation.deploys.editor.tasks.install_editor import install_editor
-# from workstation.deploys.languages.tasks.install_languages import install_languages
-# from workstation.deploys.ssh.tasks.install_ssh import install_ssh
-# from workstation.deploys.utilities.tasks.install_utilities import install_utilities
-# from workstation.deploys.workflow.tasks.install_workflow import install_workflow
-# from pyinfra import host
+from deploys.terminal.tasks.install_terminal import install_terminal
 
-# if host.data.get("install_terminal"):
-#     install_terminal()
+# from deploys.editor.tasks.install_editor import install_editor
+# from deploys.languages.tasks.install_languages import install_languages
+# from deploys.ssh.tasks.install_ssh import install_ssh
+# from deploys.utilities.tasks.install_utilities import install_utilities
+# from deploys.workflow.tasks.install_workflow import install_workflow
+from pyinfra import host
+
+if host.data.get("install_terminal"):
+    install_terminal()
 
 # if host.data.get("install_editor"):
 #     install_editor()
