88f3073810b58c424afbeace0a1de829f78044d3
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2024-11-19 21:01:46 +0100

    basic cli

--- release/source/commands/index.tsx
+++ release/source/commands/index.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Text } from "ink";
 import zod from "zod";
+import { releaseVersion } from "nx/release/index.js";
 
 export const options = zod.object({
 	name: zod.string().default("Stranger").describe("Name"),
@@ -10,7 +11,20 @@ type Props = {
 	options: zod.infer<typeof options>;
 };
 
+// import the nx release packages
+// run the version, changelog and publish commands with dry run
+// see what happens
+
 export default function Index({ options }: Props) {
+	useEffect(() => {
+		const run = async () => {
+			const result = await releaseVersion({ dryRun: true, verbose: true });
+			console.log(result);
+		};
+
+		run();
+	}, []);
+
 	return (
 		<Text>
 			Hello, <Text color="green">{options.name}</Text>
