43e62c0d7a37e1bf64dec68314c59b9e4ca95bc4
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2024-11-20 20:55:52 +0100

    Properly call function with stdout / stderr separated

--- release/source/repositories/VersionRepository.ts
+++ release/source/repositories/VersionRepository.ts
@@ -1,20 +1,43 @@
+import childProcess from "node:child_process";
+// import type Buffer from "node:buffer";
+import { join } from "path";
+
 export default class VersionRepository {
 	cliDirectory: string;
+	releaseVersionFile: string;
 
 	constructor(cliDirectory: string) {
 		this.cliDirectory = cliDirectory;
+		this.releaseVersionFile = join(cliDirectory, "utils", "release-version.js");
 	}
 
 	async applyVersion(dryRun: boolean, verbose: boolean): Promise<void> {
-		console.log("Applying version...");
+		// console.log("Applying version...");
 		console.log("Dry run: ", dryRun);
 		console.log("Verbose: ", verbose);
 
-		const currentWorkingDirectory = process.cwd();
-		console.log("Current working directory: ", currentWorkingDirectory);
-		console.log("Current CLI directory: " + this.cliDirectory);
+		// const currentWorkingDirectory = process.cwd();
+		// console.log("Current working directory: ", currentWorkingDirectory);
+		// console.log("Current CLI directory: " + this.cliDirectory);
+
+		const subProcess = childProcess.spawn("node", [this.releaseVersionFile]);
+
+		subProcess.stdout.on("data", (data: Buffer) => {
+			console.log("stdout");
+			// process.stdout.write(data.toString());
+			console.log(data.toString());
+		});
+
+		subProcess.stderr.on("data", (data: Buffer) => {
+			console.log("stderr");
+			// process.stdout.write(data.toString());
+			console.log(data.toString());
+		});
+
+		subProcess.on("close", (code: number) => {
+			console.log(`child process exited with code ${code}`);
+		});
 	}
 }
 
-// - find a way to call the method
 // - print stderr into separate flexbox component
