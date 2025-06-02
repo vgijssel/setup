091ba143867085b69a629e9f020755e96adabaef
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2025-06-02 20:01:08 +0200

    Add more dotfiles

--- stacks/devenv/files/dot_finicky.js
+++ stacks/devenv/files/dot_finicky.js
@@ -7,6 +7,13 @@ export default {
         name: "Google Chrome",
         profile: "Profile 2"
       }
+    },
+    {
+      match: "*.inverselink.com/*",
+      browser: {
+        name: "Google Chrome",
+        profile: "Profile 2"
+      }
     }
   ]
 }
