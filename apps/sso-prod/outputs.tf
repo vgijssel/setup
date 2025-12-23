58f4a9299e418b5301739b9e7a2bf47d980dc6b7
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2025-12-23 13:29:07 +0000

    feat(repo): Add client_secret output for verification
    Task: 1
    Phase: COMMIT
    Tests: 4 passing, 0 failing

--- apps/sso-prod/outputs.tf
+++ apps/sso-prod/outputs.tf
@@ -1,2 +1,22 @@
 # Outputs for Keycloak OIDC Client
-# This file will be populated in subtask 1.5
+
+output "client_id" {
+  description = "The OIDC client ID for Coder"
+  value       = keycloak_openid_client.coder.client_id
+}
+
+output "client_secret" {
+  description = "The OIDC client secret for Coder (use 'nx output' to view)"
+  value       = keycloak_openid_client.coder.client_secret
+  sensitive   = true
+}
+
+output "issuer_url" {
+  description = "The OIDC issuer URL for the cozy realm"
+  value       = "https://keycloak.enigma.vgijssel.nl/realms/cozy"
+}
+
+output "onepassword_item_id" {
+  description = "The 1Password item ID where credentials are stored"
+  value       = onepassword_item.coder_oidc_credentials.id
+}
