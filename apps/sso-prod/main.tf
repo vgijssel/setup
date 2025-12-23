e249a2db7ee0d103181023a9f045a96cc95ce48a
mvgijssel <6029816+mvgijssel@users.noreply.github.com>
2025-12-23 13:26:02 +0000

    feat(repo): Create keycloak_openid_client resource for 'coder'
    Task: 1
    Phase: COMMIT
    Tests: 9 passing, 0 failing

--- apps/sso-prod/main.tf
+++ apps/sso-prod/main.tf
@@ -1,2 +1,29 @@
 # Keycloak OIDC Client Configuration for Coder
-# This file will be populated in subtask 1.3
+
+# Data source to reference the existing cozy realm
+data "keycloak_realm" "cozy" {
+  realm = "cozy"
+}
+
+# OIDC Client for Coder
+resource "keycloak_openid_client" "coder" {
+  realm_id  = data.keycloak_realm.cozy.id
+  client_id = "coder"
+  name      = "Coder"
+  enabled   = true
+
+  access_type              = "CONFIDENTIAL"
+  standard_flow_enabled    = true
+  direct_access_grants_enabled = false
+  service_accounts_enabled = false
+
+  valid_redirect_uris = [
+    "https://coder.enigma.vgijssel.nl/*"
+  ]
+
+  web_origins = [
+    "https://coder.enigma.vgijssel.nl"
+  ]
+
+  login_theme = "keycloak"
+}
